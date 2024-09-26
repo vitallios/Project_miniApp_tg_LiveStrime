3f6fad70f3573f7ef76765e00197e2633a7a2353
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-09-26 14:42:25 +0300

    add chenal

--- script.js
+++ script.js
@@ -2,31 +2,69 @@ const navBTN = document.querySelector('#menu-btn');
 const menu = document.querySelector('.menu');
 const menuListItem = document.querySelector('#menu__list-item');
 const menuList = document.querySelector('#menu__list');
+let iframeSeaction = document.querySelector('#videoPleer');
 
+const key = '04cf7fba122224318f217fe6344845db'
+const url = `https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?`
+
+const myHeaders = new Headers();
+myHeaders.append("x-rapidapi-key", `${key}`);
+myHeaders.append("x-rapidapi-host", "v1.rugby.api-sports.io");
+
+let requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+// fetch("https://v1.rugby.api-sports.io/leagues", requestOptions)
+//   .then(response => {
+//    if (response.status == 200) {
+//     response.json()
+//     .then(data => {
+//       data.response.forEach(item => {
+//        console.dir(item);
+//        return item;
+//       });
+//     })
+//    }
+//   }  )
+//   .catch(error => console.log('error', error));
 
 const catalogLinks = [
  {
-  Id: 1,
-  name: 'video1',
-  link: '#video1',
+  id: 1,
+  name: 'Regby',
+  link: 'https://rutube.ru/play/embed/0369d61bfb8ce126d15fada863cfd08f',
+  // link: '2',
  },
  {
   id: 2,
-  name: 'video2',
-  link: '#video2',
- },
- {
-  id: 3,
-  name: 'video3',
-  link: '#video3',
- },
- {
-  id: 4,
-  name: 'video4',
-  link: '#video4',
+  name: 'Hockey',
+  link: 'https://rutube.ru/play/embed/7a485ef5da9fe50ba939f36ca3e6ed96',
+  // link: '1',
  },
 ]
 
+// fetch(`https://v1.${sport}api-sports.io/`, requestOptions)
+//   .then(response => {
+//    console.dir(response);
+//    if (response.status == 200) {
+//     response.json()
+//     .then(data => {
+//      console.dir(data);
+//       data.response.forEach(item => {
+//       console.dir(item.seasons);
+//       const li = document.createElement('li');
+//       li.classList.add('menu__list-item');
+//       li.innerHTML = `<a class="menu__list-link" id="${item.id}" href="${item.link}" name="${item.name}">${item.name}</a>`;
+//       menuList.appendChild(li);
+//       }
+//       );
+//     })
+//    }
+//   }  )
+//   .catch(error => console.log('error', error));
 
 
 
@@ -36,9 +74,21 @@ navBTN.addEventListener('click', () => {
  ? 'M6 18L18 6M6 6l12 12' : 'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5';
 });
 
+
+
+
 catalogLinks.forEach(item => {
  const li = document.createElement('li');
  li.classList.add('menu__list-item');
- li.innerHTML = `<a class="menu__list-link" href="${item.link}" name="${item.name}">${item.name}</a>`;
+ li.innerHTML = `<button class="menu__list-link" id="${item.id}" name="${item.name}">${item.name}</butt>`;
  menuList.appendChild(li);
+
+ li.firstChild.addEventListener('click', () => {
+  if(li.firstChild.id == `${item.id}`) {
+   iframeSeaction.src = `${item.link}`;
+  }
 });
+
+
+// iframeSeaction.src});
+})
