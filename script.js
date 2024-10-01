0fcc1db92d43d6dc4643a75a65603ce101a0aadb
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-10-01 11:30:40 +0300

    function play to data

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let wrapPleer = document.querySelector(".wrap");
 const loader = document.querySelector(".loader");
 const rundomFilms = document.querySelector("#rundomFilms");
 const btnToHome = document.querySelector("#btnToHome");
+const MenuBtnToHome = document.querySelector("#MenuBtnToHome");
 // Тв программы
 const catalogLinks = [
   {
@@ -63,7 +64,7 @@ const transLinks = [
     id: 2,
     name: "Трансляция2",
     link: "https://rutube.ru/play/embed/edf4662bfdf97433ced02ab6154313c4",
-    data: "2024.10.05",
+    data: "2024.10.06",
     time: "14:00",
     img: "./img/im2.jpg",
   },
@@ -71,8 +72,8 @@ const transLinks = [
     id: 3,
     name: "Трансляция3",
     link: "https://rutube.ru/play/embed/f893c0764662db0b17b277d15d6e0871",
-    data: "2024.10.05",
-    time: "14:00",
+    data: "2024.10.01",
+    time: "10:50",
     img: "./img/im3.jpg",
   },
   {
@@ -80,7 +81,7 @@ const transLinks = [
     name: "Трансляция4",
     link: "https://rutube.ru/play/embed/0b87b1557018d8ff86c86cf3492ad5a8",
     data: "2024.10.05",
-    time: "14:00",
+    time: "19:00",
     img: "./img/im4.jpg",
   },
 ];
@@ -97,7 +98,7 @@ function openVideoIFrame(linkVideo) {
 }
 
 // загрузка контента
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("load", () => {
     document.querySelector(".content").style.opacity = "0";
 
@@ -112,6 +113,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // при нажатии, открывается home страница
   btnToHome.addEventListener("click", () => {
     document.querySelector(".wrap__pages").classList.add("info__none");
+  });
+
+  MenuBtnToHome.addEventListener("click", () => {
+    window.location.reload();
   });
 
   // при нажатии, запускается рандомный фильм из списка
@@ -153,13 +158,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const h3 = document.createElement("h3");
     const span = document.createElement("span");
     h3.textContent = link.name;
-    span.textContent = link.data;
+    span.textContent = `${link.data} - ${link.time}`;
     button.appendChild(h3);
     button.appendChild(span);
     button.classList.add("slide");
     button.setAttribute("data-slide-to", index);
     button.setAttribute("id", index);
-    button.style.backgroundImage = `url('${link.img}')`;
+    button.setAttribute("data-time", link.time);
+    button.setAttribute("data-link", link.link);
+    button.setAttribute("data-data", link.data);
+    button.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgb(21 254 255 / 40%)),url('${link.img}')`;
     slides.appendChild(button);
 
     const spanNav = document.createElement("span");
@@ -198,6 +206,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
   leftArrow.addEventListener("click", prevSlide);
   rightArrow.addEventListener("click", nextSlide);
+
+// });
+
+const sliderItem = document.querySelectorAll(".slide");
+sliderItem.forEach((item) => {
+  // console.dir(item.getAttribute("data-time"));
+  // console.dir(item.getAttribute("data-data"));
+
+  let Data = new Date;
+  const hours = Data.getHours();
+  const minutes = Data.getMinutes();
+  const time = `${hours}:${minutes}`;
+
+  // let newTime = item.getAttribute("data-time") === time ? time : '00:00'; 
+
+  const day = `${Data.getUTCFullYear()}.${(Data.getUTCMonth() + 1) > 9 ? (Data.getUTCMonth() + 1) : '0' + (Data.getUTCMonth() + 1)}.${(Data.getDay() - 1) > 9 ? (Data.getDay() - 1) : '0' + (Data.getDay() - 1)}`
+
+
+  if (`${item.getAttribute("data-time")}` >= `${time}`) {
+    console.log('non');
+  } else {
+    item.children[1].textContent = "Трансляция началась ";
+    item.children[1].style.background = "#008000a6";
+    item.children[1].style.fontSize = "18px";
+    item.children[1].style.fontWeight = "bold";
+    item.children[1].style.padding = "5px 10px";
+    item.children[1].style.borderRadius = "10px";
+    item.children[1].style.marginTop = "10px";
+    item.classList.add("active-baner");
+    item.addEventListener("click", () => {
+      const link = item.getAttribute("data-link");
+      openVideoIFrame(link);
+    });
+    // item.classList.remove("active-baner");
+  }
+
+
+  console.log(`${item.getAttribute("data-time")} === ${time}`);
+  console.log(`${item.getAttribute("data-data")} === ${day}`);
+
+
+
+
 });
 
 // if (window.innerWidth < 900 && window.userAgentData.mobile) {
