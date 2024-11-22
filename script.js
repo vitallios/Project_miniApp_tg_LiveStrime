12d09f868e36ae21b9920270b5c67978c81b28fa
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-11-22 18:08:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ let transLinks = [
   },
   {
     id: 2,
-    name: `Франция — Аргентина | Летние тесты | Комментор - Семён Крюков`,
+    name: `Регби: Франция — Аргентина | Летние тесты | Комментор - Семён Крюков`,
     link: `<iframe src="https://vk.com/video_ext.php?oid=-15550428&id=456244601&hash=17e60939fc942151" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
     data: "2024.11.22",
     time: "22:00",
