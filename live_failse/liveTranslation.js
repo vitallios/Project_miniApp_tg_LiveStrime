2964c87b18547b584f24a0197e1b51c2cffefb54
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-01-24 11:23:21 +0300

    update list translation

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,55 +6,15 @@
 export const transLinks = [
   {
     id: 1,
-    name: `«Эдинбург» — «Глазго»`,
-    link: `<iframe src="https://vk.com/video_ext.php?oid=-42170580&id=456244111&hash=d1fc3f0074dd8d22" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2024.12.28",
-    time: "18:00",
-    img: "https://sun1-54.userapi.com/impg/UK3ktzWdUEgsnCjDJ5R1GRbVKP2tojtjRD1dwQ/LKVflYI3L_E.jpg?size=1280x720&quality=95&sign=eb10bf68cf2c33c26d48642346c75e67&type=video_thumb",
-    premium: '0',
+    name: `«Мировая серия по регби-7. 3 тур. День 1. 24.01.2025»`,
+    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277052&hash=a037a58329d002c0" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
+    data: "2025.01.24",
+    time: "10:00",
+    img: "https://sun1-96.userapi.com/impg/H6WAO5Q7qOlDbyk6Rpj5g1P6w9swdBhCsDxi2g/63IwBE9fGgI.jpg?size=320x180&amp;quality=95&amp;sign=cd1ba875daa775f0d3be6dddb50b46f8&amp;c_uniq_tag=55BxZ04PLZPFApOqaasJrkP70MYhZ0KPCp6tV4x14LA&amp;type=video_thumb",
+    premium: 'Premium',
     active: 0,
   },
-  {
-    id: 2,
-    name: `«Арлекинс» — «Лестер»`,
-    link: `<iframe src="https://vk.com/video_ext.php?oid=-42170580&id=456244112&hash=5877c419291966a8" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2024.12.28",
-    time: "21:00",
-    img: "https://sun9-56.userapi.com/impg/9FJ5L62Jzh7ZkrqsWKxSP0BsJcytGWp_QmS1ag/TOsKSQLQHvo.jpg?size=1280x720&quality=95&sign=17b7f61c56f25040a424dddae7e1a741&type=video_thumb",
-    premium: '0',
-    active: 0,
-  },
-  // 28.12.2024
-  {
-    id: 3,
-    name: `«Эксетер» — «Глостер»`,
-    link: `<iframe src="https://vk.com/video_ext.php?oid=-42170580&id=456244113&hash=40c06d13bed28a10" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2024.12.29",
-    time: "18:00",
-    img: "https://sun1-47.userapi.com/impg/IBmiYhqwfanTR4bRJIddJYVR-eyMXgP7GpNyEg/QQsf82hAMsY.jpg?size=1280x720&quality=95&sign=9b8cf13b713be64f364bbd55d3d03af4&type=video_thumb",
-    premium: '0',
-    active: 0,
-  },
-  {
-    id: 4,
-    name: `«Тулуза» — «Стад Франсе»`,
-    link: `<iframe src="https://vk.com/video_ext.php?oid=-42170580&id=456244114&hash=08caac43505528ce" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2024.12.29",
-    time: "23:05",
-    img: "https://sun1-95.userapi.com/impg/URh_jzENd_a6DhLDupfuGXn8dGANBwwb1HWXKg/JDuN2ss9fow.jpg?size=1280x720&quality=95&sign=e3cdc943a1b565976e7654acea6fe0e4&type=video_thumb",
-    premium: '0',
-    active: 0,
-  },
-  {
-    id: 4,
-    name: `«Тулуза» — «Стад Франсе»`,
-    link: `<iframe src="https://vk.com/video_ext.php?oid=-42170580&id=456244114&hash=08caac43505528ce" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2024.12.30",
-    time: "00:05",
-    img: "https://sun1-95.userapi.com/impg/URh_jzENd_a6DhLDupfuGXn8dGANBwwb1HWXKg/JDuN2ss9fow.jpg?size=1280x720&quality=95&sign=e3cdc943a1b565976e7654acea6fe0e4&type=video_thumb",
-    premium: '0',
-    active: 0,
-  },
+  
 
 
 ];
