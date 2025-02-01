0874bce0776d06399f4ac2c47472d36be4d4a2d7
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-02-01 09:16:19 +0300

    Update liveTranslation.js

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,11 +6,11 @@
 export const transLinks = [
   {
     id: 1,
-    name: `«Мировая серия по регби-7. 3 тур. День 1. 24.01.2025»`,
-    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277052&hash=a037a58329d002c0" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    data: "2025.01.24",
+    name: `Кубок России по регби на снегу среди мужских команд`,
+    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-40984897&id=456243545&hash=4b3b133dc6b5dd11" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
+    data: "2025.02.01",
     time: "10:00",
-    img: "https://sun1-96.userapi.com/impg/H6WAO5Q7qOlDbyk6Rpj5g1P6w9swdBhCsDxi2g/63IwBE9fGgI.jpg?size=320x180&amp;quality=95&amp;sign=cd1ba875daa775f0d3be6dddb50b46f8&amp;c_uniq_tag=55BxZ04PLZPFApOqaasJrkP70MYhZ0KPCp6tV4x14LA&amp;type=video_thumb",
+    img: "",
     premium: 'Premium',
     active: 0,
   },
