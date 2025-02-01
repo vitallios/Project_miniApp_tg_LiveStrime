57f3939c1e457ad613fa7851a3c9ac6716f07029
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-02-01 15:50:42 +0300

    Update liveTranslation.js

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -9,7 +9,7 @@ export const transLinks = [
     name: `Кубок России по регби на снегу среди мужских команд`,
     link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-40984897&id=456243545&hash=4b3b133dc6b5dd11" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
     data: "2025.02.01",
-    time: "10:00",
+    time: "15:50",
     img: "",
     premium: 'Premium',
     active: 0,
