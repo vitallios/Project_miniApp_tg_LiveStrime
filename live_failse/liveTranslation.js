f91f743366043646faec17078aa60e560ed24fe4
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-02-28 12:11:58 +0300

    add live

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -4,42 +4,45 @@
 // 1 - трансляция началась
 
 export const transLinks = [
-  // {
-  //   id: 1,
-  //   name: `«Кастр» – «Лион»`,
-  //   link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277803&hash=bf1377b8e4bf30d0" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-  //   data: "2025.02.25",
-  //   time: "12:30",
-  //   img: "https://sun9-16.userapi.com/impg/YGErBpSt-L-FsrcU-nT7mGhg2BlZ0GnUxFAUqw/yP3kwYiqgw0.jpg?size=1280x721&quality=95&sign=066b1e86b6de8fa61e938d77f92a90ee&type=video_thumb",
-  //   premium: "Premium",
-  //   active: 0,
-  // },
-  // {
-  //   id: 2,
-  //   name: `«Уэльс» –«Ирландия»`,
-  //   link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-42170580&id=456244174&hash=7b1fe8b817fd10f1" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-  //   data: "2025.02.25",
-  //   time: "13:15",
-  //   img: "https://sun1-97.userapi.com/impg/NE8VDr7sVJuAfJ_kXx_GsmJ9D5jJDXRuI1BJFg/liz8hTdwMSs.jpg?size=1280x720&quality=95&sign=cb88416585e178560fd80a8629413166&type=video_thumb",
-  //   premium: "",
-  //   active: 0,
-  // },
-  // {
-  //   id: 3,
-  //   name: `«Ля Рошель» – «Расинг»`,
-  //   link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277804&hash=51548d93fdc9f9ba" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-  //   data: "2025.02.25",
-  //   time: "18:30",
-  //   img: "https://sun9-21.userapi.com/impg/HtCVegQL7Nl0o-1Q-fKYdcWj62Hvy4fbeRbxbA/fkQaR_HJxD0.jpg?size=1280x718&quality=95&sign=dd2c4b27c6c147f987b3ee67d3c8fd5f&type=video_thumb",
-  //   premium: "",
-  //   active: 0,
-  // },
+  {
+    id: 1,
+    name: `Про Д2. Коломье - Брив`,
+    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277947&hash=641364c87d17fdf1" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
+    data: "2025.02.28",
+    time: "21:00",
+    img: "https://sun9-15.userapi.com/impg/iW4pdzxZ8dbXducs3dm8spt_Jq6a1xNgBOm_tw/ILP0vWzczSI.jpg?size=1280x720&quality=95&sign=71200074189e96684c0187c2bb8680c4&type=video_thumb",
+    premium: "",
+    active: 0,
+  },
+  {
+    id: 2,
+    name: `Про Д2. Ангулем - Орийяк`,
+    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277948&hash=5b0e57b904dc3610" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
+    data: "2025.02.28",
+    time: "21:30",
+    img: "https://sun1-91.userapi.com/impg/vesVISVDRZewov6sPI4UKihnfa-_WyWWWNGIig/oTxrHwd3PaU.jpg?size=1280x717&quality=95&sign=336a37aa15da473b6f05866ca11dd8db&type=video_thumb",
+    premium: "",
+    active: 0,
+  },
+  {
+    id: 3,
+    name: `Про Д2. Гренобль - Безье`,
+    link: `<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456277949&hash=93d67b6c7d4b483f" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
+    data: "2025.02.28",
+    time: "23:00",
+    img: "https://sun9-16.userapi.com/impg/IYgMRB0p1HY9UAZx3LkaaqnQAtpGtOScsziruw/MvBGxLAcxCQ.jpg?size=1280x714&quality=95&sign=62cd47304c7ad992bc632d98cb0c6b6e&type=video_thumb",
+    premium: "",
+    active: 0,
+  },
+  // 
+
+  
   
   // {
   //   id: 6,
   //   name: ``,
   //   link: ``,
-  //   data: "",
+  //   data: "2025.02.28",
   //   time: "",
   //   img: "",
   //   premium: "",
