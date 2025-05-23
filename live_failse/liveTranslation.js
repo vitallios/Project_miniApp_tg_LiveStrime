276c957a70f7a2bc140f1d2fadb2aacf5b682979
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-05-23 16:07:54 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -5,7 +5,47 @@
 
 export const transLinks = [
   {
-    "category": "теннис",
+    "category": "🏓 Гольф",
+    "name": "Гольф. Charles Schwab Challenge 🇺🇸 США. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"378\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/evr-2\" width=\"620\"></iframe>",
+    "data": "2025.05.23",
+    "time": "23:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/01/Гольф.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "🚴 Велоспорт",
+    "name": "Велоспорт. Вуэльта Бургоса 2025. 2-й этап. Женщины. Прямая 23 мая 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239555&amp;hash=57ca1a9a9b33f9fe\" width=\"640\"></iframe>",
+    "data": "2025.05.23",
+    "time": "15:30",
+    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "⚽️ Футбол",
+    "name": "Эгриси – ФК Матч. Медиалига. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242426&amp;hash=7117d36a7a530161\" width=\"640\"></iframe>",
+    "data": "2025.05.23",
+    "time": "21:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "⚽️ Футбол",
+    "name": "ФК Банка – Fight Nights. Медиалига. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242425&amp;hash=246dbd367f843f02\" width=\"640\"></iframe>",
+    "data": "2025.05.23",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "🎾 Теннис",
     "name": "Феликс Оже-Альяссим – Андрей Рублёв. 1/2 финала ATP 500 🇩🇪 Гамбург. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456242609&amp;hash=557833d00fe94b37\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -15,7 +55,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "🇦🇷 Томас Мартин Этчеверри – 🇮🇹 Флавио Коболли. 1/2 финала ATP 500 🇩🇪 Гамбург. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456242607&amp;hash=2fb02c8c2bc88f11\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -25,7 +65,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "🇷🇴 Жаклин Кристиан – 🇨🇴 Мария Камила Осорио Серрано. 1/2 финала WTA 250 🇲🇦 Рабат. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246028&amp;hash=ea3f9294897ebccb\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -35,7 +75,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "🇦🇺 Айла Томлянович – 🇦🇺 Майя Джойнт. 1/2 финала WTA 250 🇲🇦 Рабат. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246028&amp;hash=ea3f9294897ebccb\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -45,7 +85,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "Даниэль Коллинз – Людмила Самсонова. 1/2 финала WTA 500 🇫🇷 Страсбург. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246031&amp;hash=804ef2e0e590cb61\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -55,7 +95,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "Елена Рыбакина – Беатрис Хаддад Майя. 1/2 финала WTA 500 🇫🇷 Страсбург. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246030&amp;hash=e784c9d9100f3aef\" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -65,7 +105,7 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
+    "category": "🎾 Теннис",
     "name": "🇨🇳 Синью Янь/🇹🇼 Фан-сянь У – 🇨🇳 Ханью Гуо/🇺🇸 Николь Мелихар-Мартинес. 1/2 финала WTA 500 🇫🇷 Страсбург. Прямая",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/6QI4BcRF62T7 \" width=\"640\"></iframe>",
     "data": "2025.05.23",
@@ -75,75 +115,25 @@ export const transLinks = [
     "active": 0
   },
   {
-    "category": "теннис",
-    "name": "🇺🇦 Анастасия Соболева – 🇬🇧 Франческа Джонс. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246328&amp;hash=0dd208a9b34f7833\" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "13:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
+    "category": "🏉 Регби",
+    "name": "4 тур Чемпионата России по регби-7 среди женских команд в Красноярске. Первый игровой день",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-40984897&amp;id=456243737&amp;hash=e6b8e534d9e39c75\" width=\"640\"></iframe>",
+    "data": "2025.05.24",
+    "time": "10:00",
+    "img": "https://sun9-74.userapi.com/impg/PrmON5kOwDwmCEp2EkmIM_kRxSOHZ1b6UHOBgA/b9acLVaW0T4.jpg?size=1280x720&quality=95&sign=c10bca31cdaeeafbc03fb4891772a375&type=video_thumb",
     "premium": "",
     "active": 0
   },
   {
-    "category": "теннис",
-    "name": "🇵🇱 Линда Климовичова – 🇪🇸 Лейре Ромеро Гормас. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/sFiA2SHUNewk \" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "12:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
+    "category": "🏉 Регби",
+    "name": "24 мая, «Металлург» – «Стрела-Ак Барс», стадион «Металлург», Новокузнецк",
+    "link": "<iframe class=\"sb-iframe\" src=\"https://sportsru.sprinttyott.com/embed/bGJ-MhoAUOYgBkCf-wgAOjvM32XJbbTqvCKZQP1nSXY?autoplay=1&amp;loop=1&amp;muted=1\" height=\"410\" width=\"730\" loading=\"lazy\" allow=\"fullscreen: none\" type=\"iframe\" index=\"4\"></iframe>",
+    "data": "2025.05.24",
+    "time": "11:15",
+    "img": "https://st.video.sports.ru/thumbnail/preview/682dbadf41686dfa391cd076/maxres.png",
     "premium": "",
     "active": 0
   },
-  {
-    "category": "теннис",
-    "name": "🇰🇿 Александр Шевченко – 🇦🇹 Филип Мисолич. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246328&amp;hash=0dd208a9b34f7833\" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "12:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "🇺🇦 Юлия Стародубцева – 🇨🇿 Сара Бейлек. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/ApUPXLm6-L4L \" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "12:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "🇺🇸 Тейлор Таунсенд – 🇦🇺 Дарья Сэвилл. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/62udj1WaZdkW \" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "12:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Ротор-2 – Химки-М. Вторая Лига Б. Прямая 23 мая 2025 17:00.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456243762&amp;hash=4eecee62c2af2e05\" width=\"640\"></iframe>",
-    "data": "2025.05.23",
-    "time": "17:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Ингулец – Ворскла. УПЛ. Прямая 23 мая 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" sandbox=\"allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation\" src=\" https://p2plive4.top/stream2/upl2.php \" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.05.23",
-    "time": "15:30",
-    "img": "https://srrb.ru/wp-content/uploads/2020/09/футбольный-мяч.jpg",
-    "premium": "",
-    "active": 0
-  }
 ]
 
 // {
