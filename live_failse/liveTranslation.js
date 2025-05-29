56ece14e07cbef418b4e6bceb9963eb01840f1d3
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-05-29 12:36:23 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,49 +6,9 @@
 export const transLinks =[
   {
     "category": "теннис",
-    "name": "Юлия Стародубцева – Анастасия Потапова. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246364&amp;hash=831703b1a3b6e76f\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "🇭🇺 Анна Бондарь – 🇺🇦 Элина Свитолина. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246363&amp;hash=eb8b1c09da9eabd1\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Карен Хачанов – Себастьян Офнер. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246090&amp;hash=de0c2a63d06afe5d\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "13:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Каспер Рууд – Нуно Боржеш. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246087&amp;hash=384d5ce0cbb26410\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "13:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Жасмин Паолини – Айла Томлянович. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246366&amp;hash=418cf1637570dfdd\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "🇺🇸 Энн Ли – 🇺🇸 Джессика Пегула. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246372&amp;hash=8d0c0b4e0bb04266\" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "13:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -56,9 +16,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇺🇦 Ангелина Калинина/🇨🇳 Сюй Ифань – 🇭🇺 Тимея Бабош/🇧🇷 Луиза Стефани. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/ANjwLg3lKFyA \" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "🇷🇺 Ирина Хромачёва/🇭🇺 Фанни Столлар – 🇨🇳 Синьюй Ван/🇨🇳 Чжэн Сайсай. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/RTIwOyDJSNKb \" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -66,9 +26,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇨🇿 Вит Коприва – 🇩🇪 Даниэль Альтмайер. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/aYd_0mP5bzwJ \" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "🇨🇿 Мари Боузкова – 🇬🇧 Сонай Картал. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246371&amp;hash=8ffc8a97a221c054\" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -76,9 +36,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇦🇷 Хуан Мануэль Серундоло – 🇷🇸 Хамад Меджедович. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/NphR-M8ZUWg4 \" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "🇨🇿 Иржи Легечка – 🇪🇸 Алехандро Давидович Фокина. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/8hIRBOpP8uVD \" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -86,9 +46,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇨🇱 Алехандро Табило – 🇦🇺 Алексей Попырин. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246364&amp;hash=831703b1a3b6e76f\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "🇵🇱 Магдалена Фрех – 🇨🇿 Маркета Вондроушова. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246373&amp;hash=e7f45863be2ee649\" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -96,9 +56,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇩🇪 Ева Лис – 🇨🇦 Виктория Мбоко. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246365&amp;hash=da45766ad710994a\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "Энрике Роша – Якуб Меншик. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/_I2CVgg27pcG \" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -106,9 +66,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇨🇭 Виктория Голубич – 🇺🇸 Аманда Анисимова. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246361&amp;hash=d6975c427bc717c4\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "Алекс Де Минор – Александр Бублик. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/MyMtNFE53A8c \" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -116,9 +76,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇮🇹 Лоренцо Музетти – 🇨🇴 Даниэль Элахи Галан. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246363&amp;hash=eb8b1c09da9eabd1\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "Мирра Андреева – Эшлин Крюгер. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/SjLQROp6jqI5 \" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -126,9 +86,9 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "🇨🇴 Эмилиана Аранго – 🇨🇳 Чжэн Циньвэнь. Ролан Гаррос 2025. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246086&amp;hash=28de4fe55911ccce\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "Хауме Муньяр – Артур Фис. Ролан Гаррос 2025. Прямая",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246374&amp;hash=d0a6bac2dea88c3c\" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "12:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
@@ -136,44 +96,15 @@ export const transLinks =[
   },
   {
     "category": "теннис",
-    "name": "Ролан Гаррос 2025. Прямая 28 мая 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246086&amp;hash=28de4fe55911ccce\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
+    "name": "Ролан Гаррос 2025. Прямая 29 мая 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246374&amp;hash=d0a6bac2dea88c3c\" width=\"640\"></iframe>",
+    "data": "2025.05.29",
     "time": "18:45",
     "img": "https://srrb.ru/wp-content/uploads/2023/05/Roland-Garros.jpg",
     "premium": "",
     "active": 0
   },
-  {
-    "category": "футбол",
-    "name": "Волгарь – Спартак Кс. Вторая Лига А. Прямой 28 мая 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244225&amp;hash=3af2c59d6e393b05\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "18:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "футбол",
-    "name": "Машук-КМВ – Кубань. Вторая Лига А. Прямой 28 мая 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244224&amp;hash=9a6d6d18fc9f71b6\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "17:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "футбол",
-    "name": "Велес – Волга Ул. Вторая Лига А. Прямой 28 мая 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244223&amp;hash=55fbb8f3728de752\" width=\"640\"></iframe>",
-    "data": "2025.05.28",
-    "time": "17:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига.jpg",
-    "premium": "",
-    "active": 0
-  }
+
 ]
 
 
