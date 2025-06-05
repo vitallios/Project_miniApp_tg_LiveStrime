393724d12005b966321fa99baa7f67c9257fc19d
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-06-05 09:12:00 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -5,52 +5,12 @@
 
 export const transLinks =[
   {
-    "category": "другое",
-    "name": "Lit Energy – Rocket Team. 1/8 финала Медиалиги. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242753&amp;hash=bd2387f25eb50705\" width=\"640\"></iframe>",
-    "data": "2025.06.04",
-    "time": "21:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "SD Family – Эгриси. 1/8 финала Медиалиги. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242752&amp;hash=808f87039d968ece\" width=\"640\"></iframe>",
-    "data": "2025.06.04",
-    "time": "16:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Велоспорт. Тур Словении 2025. 1-й этап. Прямая 4 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239604&amp;hash=a18d7296f6c5962b\" width=\"640\"></iframe>",
-    "data": "2025.06.04",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "гандбол",
-    "name": "Пляжный гандбол. Чемпионат России 2025. Женщины. Прямая 4 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"405\" src=\" https://rushandballtv.clients.webcaster.pro/iframe/feed/start/free_65af76c9d1249104e7f2c5c43cd3fe47/6079_8542672267/5a78ace4ddebbfd0b8b1f7b0dc046871/1749072027?sr=6511&amp;type_id=&amp;width=100%25&amp;height=100%25&amp;iframe_width=100%25&amp;iframe_height=100%25&amp;lang=ru\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.04",
-    "time": "09:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/11/гандбол-россия.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "Бейсбол",
-    "name": "Сан-Франциско Джайнтс Сан-Диего Падрес. Бейсбол. Прямая 4 июня 2025 04:45.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227346189&amp;id=456241660&amp;hash=8c8d01726087e300\" width=\"640\"></iframe>",
-    "data": "2025.06.04",
-    "time": "04:45",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/MLB-МЛБ.jpg",
+    "category": "Гребной спорт",
+    "name": "Гребной спорт. 64-я Большая Московская регата. Прямая 5 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-39885733&amp;id=456239978&amp;hash=a002b9f1049952ee\" width=\"640\"></iframe>",
+    "data": "2025.06.05",
+    "time": "10:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/гребля.jpg",
     "premium": "",
     "active": 0
   }
