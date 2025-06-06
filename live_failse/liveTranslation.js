8f2800ec0284c9e86cecc019065657e11cb6becd
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-06-06 09:00:34 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -5,121 +5,21 @@
 
 export const transLinks =[
   {
-    "category": "другое",
-    "name": "Германия – Италия. Волейбол. Лига Наций 2025. Женщины. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=57007587\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.05",
-    "time": "23:30",
-    "img": "https://srrb.ru/wp-content/uploads/2024/05/Лига-Наций-2024.-Волейбол.-Женщины.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Болгария – Доминиканская Республика. Волейбол. Лига Наций 2025. Женщины. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=57007585\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.05",
-    "time": "23:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/05/Лига-Наций-2024.-Волейбол.-Женщины.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Китай – Польша. Волейбол. Лига Наций 2025. Женщины. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=57007583\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.05",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2024/05/Лига-Наций-2024.-Волейбол.-Женщины.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Lotus Music – 2DROTS. 1/8 финала Медиалиги. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242755&amp;hash=013d652d9bd75f70\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "21:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Тандем – ФК Банка. 1/8 финала Медиалиги. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456242754&amp;hash=1bb370ae38a4ff3b\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "16:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Велоспорт. Тур Британии 2025. 1-й этап. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239605&amp;hash=fdb627107df4c329\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "WRC. Ралли Сардинии. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-140964975&amp;id=456241511&amp;hash=c059963826e85689\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/04/WRC.-2025.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Гольф. RBC Canadian Open. Прямая",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"378\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/evr-2\" width=\"620\"></iframe>",
-    "data": "2025.06.05",
-    "time": "22:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/01/Гольф.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Велоспорт. Тур Словении 2025. 2-й этап. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239606&amp;hash=fed45bf260f454c1\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "гандбол",
-    "name": "Пляжный гандбол. Чемпионат России 2025. Женщины. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"405\" src=\" https://rushandballtv.clients.webcaster.pro/iframe/feed/start/free_6ab87c81cc30dbcb76c3c2444b01c81d/6079_6433620682/9b8cd10ee191e3eacd64609056eefd49/1749152835?sr=6511&amp;type_id=&amp;width=100%25&amp;height=100%25&amp;iframe_width=100%25&amp;iframe_height=100%25&amp;lang=ru\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.05",
-    "time": "09:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/11/гандбол-россия.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Гребной спорт. 64-я Большая Московская регата. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-39885733&amp;id=456239978&amp;hash=a002b9f1049952ee\" width=\"640\"></iframe>",
-    "data": "2025.06.05",
-    "time": "10:00",
+    "category": "Гребля",
+    "name": "Гребной спорт. 64-я Большая Московская регата. Прямая 6 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-39885733&amp;id=456239980&amp;hash=ac4cf9e85d77b608\" width=\"640\"></iframe>",
+    "data": "2025.06.06",
+    "time": "09:20",
     "img": "https://srrb.ru/wp-content/uploads/2023/06/гребля.jpg",
     "premium": "",
     "active": 0
   },
   {
-    "category": "другое",
-    "name": "Бельгия – Таиланд. Волейбол. Лига Наций 2025. Женщины. Прямая 5 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/sxTwczy4oE0\" title=\"Thailand 🇹🇭 vs. Belgium 🇧🇪 - Highlights | Week 1 | Women's VNL 2025\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.06.05",
-    "time": "03:00",
+    "category": "Волейбол",
+    "name": "Франция – Бельгия. Волейбол. Лига Наций 2025. Женщины. Прямая 6 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=57007593\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.06.06",
+    "time": "10:00",
     "img": "https://srrb.ru/wp-content/uploads/2024/05/Лига-Наций-2024.-Волейбол.-Женщины.jpg",
     "premium": "",
     "active": 0
