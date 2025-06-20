b806e79ed7d56d25d72040c2d1c7c79b998409a4
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-06-20 17:52:50 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -44,7 +44,88 @@ export const transLinks = [
     "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
     "premium": "",
     "active": 0
-  }
+  },
+  {
+    "category": "Регби",
+    "name": "Топ-14. Полуфинал. Тулуза - Байон.",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456281291&hash=afe8f52cdb564b54" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.06.20",
+    "time": "22:10",
+    "img": "https://sun9-25.userapi.com/impg/47MWR0GYGRl5XWM1TEss0AcN_arS6B22rutlpQ/T2DFAx86nH4.jpg?size=1280x720&quality=95&sign=463da9d21c1371a25e8026c8e9f9525d&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "2-й тур Чемпионата Высшей лиги по регби-7 из Санкт-Петербурга. Первый день",
+    "link": '<iframe src="https://vk.com/video_ext.php?oid=-40984897&id=456243812&hash=7e12b1f3221bf4a9" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.06.21",
+    "time": "10:00",
+    "img": "https://sun9-4.userapi.com/impg/dpZA8U8861xnT_tsIj2qRsZEgMOX3ksbf3lR8A/0mvbd2vl3dg.jpg?size=1280x720&quality=95&sign=7b0fa7be0d88f6d4b37030b89e0a3a3f&type=video_thumb",
+    "premium": "premium",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "Металлург» – ВВА",
+    "link": '<iframe class="sb-iframe" src="https://sportsru.sprinttyott.com/embed/nflJQXD8Q_TRAbEMrmgv1ohqLPu1jaVbdogWu0iPOBI?allowfullscreen=1&amp;loop=1" height="410" width="730" loading="lazy" allow="fullscreen" type="iframe" index="5"></iframe>',
+    "data": "2025.06.21",
+    "time": "12:00",
+    "img": "https://st.video.sports.ru/thumbnail/preview/6852eb9dcb361998724de47c/maxres.png",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "Сборная Красноярского края - Сборная Поволжья | Чемпионат России по регби среди женских команд",
+    "link": '<iframe src="https://vk.com/video_ext.php?oid=-40984897&id=456243814&hash=0d731b13ae12d8e8" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.06.21",
+    "time": "14:00",
+    "img": "https://sun1-84.userapi.com/impg/fy5O3J4Wa12nUZcvixJ9hDhOU4eeDG3aQrmDrw/FOQvevzonKk.jpg?size=1280x720&quality=95&sign=3288a451b599c05dac005e2046edce03&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "Сборная Москвы – «ВВА-Подмосковье» | Чемпионат России по регби среди женских команд",
+    "link": '<iframe src="https://vk.com/video_ext.php?oid=-40984897&id=456243815&hash=fe4c08178e899ed4" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.06.21",
+    "time": "16:00",
+    "img": "https://sun1-90.userapi.com/impg/ug28oNuTO9fGKN7vg9OYo7vI1SgA2j8Xyk_GPw/9MLeNftxk98.jpg?size=1280x720&quality=95&sign=2f91fb792e7bb1ae5c079f0321d26b35&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": " «Локомотив» – «Слава» / 14:00 ",
+    "link": '<iframe class="sb-iframe" src="https://sportsru.sprinttyott.com/embed/bQ_VYZ75TxIS8MwxsaFbMq4bbUhwtWktPiukU8CUwG0?allowfullscreen=1&amp;loop=1" height="410" width="730" loading="lazy" allow="fullscreen" type="iframe" index="5"></iframe>',
+    "data": "2025.06.22",
+    "time": "14:00",
+    "img": "https://st.video.sports.ru/thumbnail/preview/6852ebd9a1c1f6d9dbceb175/maxres.png",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "«Динамо» – «Стрела» ",
+    "link": '<iframe class="sb-iframe" src="https://sportsru.sprinttyott.com/embed/MGVfT79MGGJbaKa19DS67tQYb050d-dh_4Eh48Sqdx4?allowfullscreen=1&amp;loop=1" height="410" width="730" loading="lazy" allow="fullscreen" type="iframe" index="5"></iframe>',
+    "data": "2025.06.22",
+    "time": "15:50",
+    "img": "https://st.video.sports.ru/thumbnail/preview/6852ec0acb361998724e8004/maxres.png",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Регби",
+    "name": "«Енисей» – «Красный Яр»  ",
+    "link": '<iframe class="sb-iframe" src="https://sportsru.sprinttyott.com/embed/SHqd1vbVShxVUxn6H25G2E6IwNAgT1F-4rcGrH4LYKE?allowfullscreen=1&amp;loop=1" height="410" width="730" loading="lazy" allow="fullscreen" type="iframe" index="5"></iframe>',
+    "data": "2025.06.23",
+    "time": "14:30",
+    "img": "https://st.video.sports.ru/thumbnail/preview/6852ec28a1c1f6d9dbcf2841/maxres.png",
+    "premium": "",
+    "active": 0
+  },
+
 
 ]
 
