b3f9f9abdddccc798bc566cc2c6e264e6b4f4162
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-06-20 16:14:03 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -7,9 +7,9 @@
 export const transLinks = [
   {
     "category": "Велоспорт",
-    "name": "Смотреть Велоспорт. Тур Бельгии. Этап 2. Прямая трансляция 19 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239648&amp;hash=35475bf67b228eeb\" width=\"640\"></iframe>",
-    "data": "2025.06.19",
+    "name": "Смотреть Велоспорт. Тур Бельгии. Этап 3. Прямая трансляция 20 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239654&amp;hash=c9ad55a02effe321\" width=\"640\"></iframe>",
+    "data": "2025.06.20",
     "time": "16:30",
     "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
     "premium": "",
@@ -17,19 +17,9 @@ export const transLinks = [
   },
   {
     "category": "Велоспорт",
-    "name": "Смотреть Велоспорт. Рут д’Окситания. Этап 2. Мужчины. Прямая трансляция 19 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239647&amp;hash=872b4f818a401eaa\" width=\"640\"></iframe>",
-    "data": "2025.06.19",
-    "time": "15:15",
-    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "Велоспорт",
-    "name": "Смотреть Велоспорт. Тур Швейцарии. Этап 5. Прямая трансляция 19 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239646&amp;hash=e4f32ba3f25e3651\" width=\"640\"></iframe>",
-    "data": "2025.06.19",
+    "name": "Смотреть Велоспорт. Тур Швейцарии. Этап 6. Прямая трансляция 20 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239653&amp;hash=419b29cf06dde8d0\" width=\"640\"></iframe>",
+    "data": "2025.06.20",
     "time": "15:50",
     "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
     "premium": "",
@@ -37,35 +27,24 @@ export const transLinks = [
   },
   {
     "category": "Велоспорт",
-    "name": "Смотреть Велоспорт. Джиро Некст Ген/Giro d’Italia Next Gen. Этап 5. Мужчины. Прямая трансляция 19 июня 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239645&amp;hash=9b1ddb3cf285519c\" width=\"640\"></iframe>",
-    "data": "2025.06.19",
-    "time": "14:30",
+    "name": "Смотреть Велоспорт. Рут д’Окситания. Этап 3. Мужчины. Прямая трансляция 20 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239652&amp;hash=d95fb8cbced97447\" width=\"640\"></iframe>",
+    "data": "2025.06.20",
+    "time": "15:05",
     "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
     "premium": "",
     "active": 0
   },
   {
-    "category": "Футбол",
-    "name": "Чемпионат Европы до 19 лет Черногория - Испания",
-    "link": `<iframe src="https://vkvideo.ru/video_ext.php?oid=-21620613&id=456251198&hash=f7ccb253c5e3c992" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    "data": "2025.06.19",
-    "time": "19:50",
-    "img": "https://sun1-23.userapi.com/impg/C7_W4cLXE3xsjJjWytIDHQz0YN7Cqpn4EoEReg/OkqYkeOCKzc.jpg?size=1280x720&quality=95&sign=0c698396da7db8ec1e9d8d9ad19ce055&type=video_thumb",
+    "category": "Велоспорт",
+    "name": "Смотреть Велоспорт. Джиро Некст Ген/Giro d’Italia Next Gen. Этап 6. Мужчины. Прямая трансляция 20 июня 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239650&amp;hash=6d3bbee96abcb13a\" width=\"640\"></iframe>",
+    "data": "2025.06.20",
+    "time": "14:20",
+    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт.jpg",
     "premium": "",
     "active": 0
-  },
-  {
-    "category": "Футбол",
-    "name": "Чемпионат Европы до 19 лет Дания - Румыния",
-    "link": `<iframe src="https://vkvideo.ru/video_ext.php?oid=-21620613&id=456251197&hash=c7d340e65aa97777" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>`,
-    "data": "2025.06.19",
-    "time": "19:55",
-    "img": "https://sun1-29.userapi.com/impg/DthLGXEpbvgzTkgseJOVQ5SaWSuLxBMLzNYmpw/kW-n0ZPG0ds.jpg?size=1280x720&quality=95&sign=ef2f23415332f29795d0346ac15bce83&type=video_thumb",
-    "premium": "",
-    "active": 0
-  },
-  
+  }
 
 ]
 
