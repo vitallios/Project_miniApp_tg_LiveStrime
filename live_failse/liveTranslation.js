c40fd773ea3faf3ad7437a2bfbf9b19127596e6f
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-05 17:10:19 +0300

    Update liveTranslation.js

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -4,86 +4,197 @@
 // 0 - трансляция закончилась
 // 1 - трансляция началась
 
-export const transLinks = [{
-    "category": "футбол",
-    "name": "Смотреть КДВ Томск – Уралец. Вторая Лига Б. Прямая трансляция 3 июля 2025 в 14:00.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244350&amp;hash=3ff83b32dfb22ae2\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
+export const transLinks = [
+  {
+    "category": "теннис",
+    "name": "Вероника Кудерметова/Элизе Мертенс – Мари Боузкова/Анна Данилина. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/gYerV0XQ7DTC \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Барбора Крейчикова – Эмма Наварро. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/kclr3uneLyqh \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Брэндон Накашима – Лоренцо Сонего. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/7_Sj4lQcwn9P \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Григор Димитров – Себастьян Офнер. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/V8sGoLgn_lbm \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Снукер. Championship League 2025. Прямая трансляция 5 июля 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/WKBiw6zisbga \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
     "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "img": "https://srrb.ru/wp-content/uploads/2025/06/Снукер.-Championship-League-2025-768x420.avif",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Краснодар – Ростов. Товарищеский матч",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-26991659&amp;id=456246078&amp;hash=b498a73fe5231f3e\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/Российская-Премьер-Лига-768x384.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Амкал – Broke Boys. Финал Медиалиги. Прямая трансляция 5 июля 2025 в 20:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456243130&amp;hash=1a3975e22cc5cbb9\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "20:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League-768x411.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "ФК 10 – Синдекат. Матч за 3-е место Медиалиги. Прямая трансляция 5 июля 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212508059&amp;id=456243129&amp;hash=6f6b9c01b04f3e54\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/06/Media-Football-League-768x411.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть ЦСКА – ОФК. Братский кубок",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2216382\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.07.05",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/05/футбольный-мяч-768x452.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Флавио Коболли – Джек Пиннингтон-Джонс. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246606&amp;hash=df3a79182033e659\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Вероника Кудерметова – Эмма Наварро. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246450&amp;hash=9e9273414357a18c\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Смотреть Уимблдон Прямая трансляция 3 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246605&amp;hash=51dfb36cd22d6817\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Бильярд. Пул. UNIVERSAL x CPBA 99 OPEN. Прямая трансляция 3 июля 2025 в 11:00.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-228871432&amp;id=456240347&amp;hash=e53df7af15abdc41\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "11:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/02/бильярд-пул-768x402.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть Женская борьба. Чемпионат Европы 2025. U20. Прямая трансляция 3 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221814623&amp;id=456240370&amp;hash=fc58dcf2d1052822\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "11:30",
-    "img": "https://srrb.ru/wp-content/uploads/2025/06/Чемпионата-Европы-2025-по-вольной-греко-римской-и-женской-борьбе-U20-768x306.avif",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть онлайн Лос-Анджелес Доджерс — Чикаго Уайт Сокс. МЛБ. Бейсбол. Прямой эфир 3 июля 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227346189&amp;id=456241963&amp;hash=71b575b8abdebd30\" width=\"640\"></iframe>",
-    "data": "2025.07.03",
-    "time": "05:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/MLB-МЛБ-768x387.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "Футбол",
-    "name": "ТОВАРИЩЕСКИЙ МАТЧ. ЗЕНИТ - ДИНАМО МАХАЧКАЛА. ПРЯМАЯ ТРАНСЛЯЦИЯ",
-    "link": '<iframe src="https://rutube.ru/static/iframe_resources/player-stub/time-to-event.html?title=%D0%A2%D0%9E%D0%92%D0%90%D0%A0%D0%98%D0%A9%D0%95%D0%A1%D0%9A%D0%98%D0%99%20%D0%9C%D0%90%D0%A2%D0%A7.%20%20%D0%97%D0%95%D0%9D%D0%98%D0%A2%20-%20%D0%9F%D0%90%D0%A0%D0%A2%D0%98%D0%97%D0%90%D0%9D.%20%20%D0%9F%D0%A0%D0%AF%D0%9C%D0%90%D0%AF%20%D0%A2%D0%A0%D0%90%D0%9D%D0%A1%D0%9B%D0%AF%D0%A6%D0%98%D0%AF&amp;future_publication=2025-07-03T19%3A00%3A00&amp;thumbnail=https%3A//pic.rutube.ru/video/2025-07-03/53/7f/537fa10e8d75bf12c5926b27623727f5.jpg" allowfullscreen=""></iframe>',
-    "data": "2025.07.03",
+    "name": "Миомир Кецманович – Новак Джокович. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246487&amp;hash=c304f4ac8bc8caa5\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
     "time": "19:00",
-    "img": "https://pic.rutubelist.ru/video/2025-07-03/91/fd/91fd7c933b4f5dd6a310d196ee28e210.jpg",
-    "premium": "premium",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
     "active": 0
   },
+  {
+    "category": "теннис",
+    "name": "Янник Синнер – Педро Мартинес Портеро. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/ieCkYeBdGea8 \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "15:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Бен Шелтон – Мартон Фучович. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246485&amp;hash=b592bf4b40b72b1d\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "18:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Ига Швентек – Даниэль Коллинз. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246486&amp;hash=979ece57606f1594\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "17:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Мирра Андреева – Хейли Баптист. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/OxhDNhjxpJm0 \" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Легкая атлетика. «Мемориал А.В. Игнатьева 2025» – «Королева российского спорта» Прямая трансляция 5 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-6783586&amp;id=456246009&amp;hash=1f2867c458ab7a7f\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": null,
+    "img": "https://srrb.ru/wp-content/uploads/2021/07/легкая-атлетика-кубок-России-Брянск-768x404.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть RCC Boxing: Курбанов vs. Сосулин. Прямая трансляция 5 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-6315911&amp;id=456242528&amp;hash=f0f7c468e5b55b6b\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/RCC-768x404.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "регби",
+    "name": "Регби. Британские и Ирландские Львы – Варатас. Прямая трансляция 5 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-15550428&amp;id=456245110&amp;hash=beef75724cc23ae5\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/08/регби-мяч-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Спортивная гимнастика. Кубок сильнейших спортсменов 2025. Финалы на видах. (1 день). Прямая трансляция 5 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-228873962&amp;id=456239339&amp;hash=b1b1d64467ee4173\" width=\"640\"></iframe>",
+    "data": "2025.07.05",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Спортивная-гимнастика.-Кубок-сильнейших-спортсменов-2025-768x396.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть DTM. Норисринг. Гонка 1. Прямая трансляция 5 июля 2025",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2216373\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.07.05",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2024/04/dtm-768x394.jpg",
+    "premium": "",
+    "active": 0
+  }
 ]
 
 
