75acc712779b250e6e921f251657f09eeaa939c5
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-08 12:22:48 +0300

    add live

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -7,206 +7,65 @@
 export const transLinks =[
   {
     "category": "теннис",
-    "name": "Тристан Скулкейт — Алекс Болт. ATP Challenger 125 Ньюпорт",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243244&amp;hash=b7e24e65620d8987\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "23:55",
-    "img": "https://srrb.ru/wp-content/uploads/2025/06/теннис-трава-768x458.avif",
+    "name": "Егор Плешивцев — Иван Иванов. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246640&amp;hash=ee01dc7f42f3088a\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Патрик Малоуни — Джеймс Дакуорт. ATP Challenger 125 Ньюпорт",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243243&amp;hash=edcd7c7ab76c4bff\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "22:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/06/теннис-трава-768x458.avif",
+    "name": "Смотреть Уимблдон 1/4 финала Прямая трансляция 8 июля 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246641&amp;hash=953f8f1aec56aa24\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Мариано Навоне — Игнасио Бусе. ATP Challenger 125 Брауншвейг",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243242&amp;hash=d0cc37b61a11235f\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "17:30",
+    "name": "Клеман Табюр — Янник Ханфманн. ATP Challenger 125 Брауншвейг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243248&amp;hash=714f87cbb7bfe28d\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "11:00",
     "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
     "premium": "",
     "active": 0
   },
   {
-    "category": "футбол",
-    "name": "Смотреть Португалия (ж) – Италия (ж). Чемпионат Европы 2025 по женскому футболу. Прямая трансляция 7 июля 2025 в 22:00",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/1fa017043aa63a620bd265b3f86c0904/\" width=\"720\"></iframe>",
-    "data": "2025.07.07",
-    "time": "22:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Европы-2025-по-женскому-футболу-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "футбол",
-    "name": "Смотреть Испания (ж) – Бельгия (ж). Чемпионат Европы 2025 по женскому футболу. Прямая трансляция 7 июля 2025 в 19:00",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/0911a0bd23b249b90e38fb1280e0fd88/\" width=\"720\"></iframe>",
-    "data": "2025.07.07",
-    "time": "19:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Европы-2025-по-женскому-футболу-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "футбол",
-    "name": "Смотреть Челябинск-2 – Уралец-ТС. Вторая Лига Б. Прямая трансляция 7 июля 2025 в 17:00.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-221212132&amp;id=456244385&amp;hash=3b446a4c700b94dd\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "17:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Янник Синнер — Григор Димитров. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246506&amp;hash=15bdf455ee0756cc\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "19:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Велоспорт. Джиро д’Италия 2025. Женщины. 2-й этап. Прямая трансляция 7 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239689&amp;hash=bf1c18d9c27628a9\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "15:45",
-    "img": "https://srrb.ru/wp-content/uploads/2024/07/Giro-dItalia-Women-768x352.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Алекс Де Минор — Новак Джокович. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/5k4DMdS6AZvW \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "15:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Ига Швентек — Клара Таусон. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246507&amp;hash=5e453e92e1ca8fe7\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "18:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Мирра Андреева — Эмма Наварро. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246505&amp;hash=dbcf6fc938a61e06\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "17:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Бен Шелтон — Лоренцо Сонего. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246504&amp;hash=dad5019a08502300\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "16:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Тимея Бабош/Луиза Стефани — Людмила Киченок/Эллен Перес. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/qVWchF2VXBT6 \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Екатерина Александрова — Белинда Бенчич. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/KIoPSVbMeCOn \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Ирина Хромачёва/Фанни Столлар — Габриэла Дабровски/Эрин Рутлифф. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/pKAs87duDPn9 \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Вероника Кудерметова/Элизе Мертенс — Беатрис Хаддад Майя/Лаура Зигемунд. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/Lrmm7RM4KscZ \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "14:30",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Людмила Самсонова — Джессика Бузас Манейро. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246503&amp;hash=ee5ae739f5873ef1\" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
     "category": "другое",
-    "name": "Смотреть Снукер. Championship League 2025. Прямая трансляция 7 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/WbJE3AYdD4wq \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/06/Снукер.-Championship-League-2025-768x420.avif",
+    "name": "Евроспорт 1. Прямой эфир смотреть онлайн",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" height=\"360 frameborder=\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/tnt1\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.08",
+    "time": null,
+    "img": "https://srrb.ru/wp-content/uploads/2024/09/ЕВРОСПОРТ-1-768x401.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "велоспорт",
-    "name": "Велогонка Тур де Франс 2025. 3-й этап. Прямая трансляция 7 июля 2025 в 14:00",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://plvideo.ru/embed/hwb-VpObUSjH\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.07.07",
+    "name": "Велогонка Тур де Франс 2025. 4-й этап. Прямая трансляция 8 июля 2025 в 14:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" height=\"360 frameborder=\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/tnt1\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.08",
     "time": "14:00",
     "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
     "premium": "",
     "active": 0
   },
   {
-    "category": "теннис",
-    "name": "Марин Чилич — Флавио Коболли. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/h5geNkQwPXdw \" width=\"640\"></iframe>",
-    "data": "2025.07.07",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "category": "другое",
+    "name": "Смотреть Милуоки Брюерс — Лос-Анджелес Доджерс. МЛБ. Бейсбол. Прямая трансляция 8 июля 2025 в 02:40.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227346189&amp;id=456242027&amp;hash=13e6cc5baf7e3d1a\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "02:40",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/MLB-МЛБ-768x387.jpg",
     "premium": "",
     "active": 0
   }
-] 
-
+]
 
 
 // git add . && git commit -m "update" && git push
