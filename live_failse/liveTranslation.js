8881fb13a4235adb0156071b0514fef46d03919a
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-08 17:56:51 +0300

    Update liveTranslation.js

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,6 +6,226 @@
 
 export const transLinks =[
   {
+    "category": "футбол",
+    "name": "Смотреть Польша (ж) – Швеция (ж). Чемпионат Европы 2025 по женскому футболу. Прямая трансляция 8 июля 2025 в 22:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/d9741ea4008ac029d891f37b39d576df/\" width=\"720\"></iframe>",
+    "data": "2025.07.08",
+    "time": "22:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Европы-2025-по-женскому-футболу-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Арина Соболенко — Лаура Зигемунд. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/VCiEfvvFEPlY \" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "15:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Тейлор Фриц — Карен Хачанов. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/0uNvL9X13uqQ \" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Германия (ж) – Дания (ж). Чемпионат Европы 2025 по женскому футболу. Прямая трансляция 8 июля 2025 в 19:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/3cc2937834da037348634ac2a7484939/\" width=\"720\"></iframe>",
+    "data": "2025.07.08",
+    "time": "19:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Европы-2025-по-женскому-футболу-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Оливия Гадеки/Дезире Кравчик — Кэролайн Доулхайд/София Кенин. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/5ZHj-obolANq\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.08",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Динамо — Ахмат. Товарищеский матч",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-83476010&amp;id=456242772&amp;hash=01e1e0b0275ac3d8\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/Российская-Премьер-Лига-768x384.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Брэндон Холт — Мёрфи Кассон. ATP Challenger 125 Ньюпорт",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243253&amp;hash=55b5100b7131e502\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "20:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/06/теннис-трава-768x458.avif",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Ясутака Учияма — Адриан Маннарино. ATP Challenger 125 Ньюпорт",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243252&amp;hash=e5ea416028ba6a78\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/06/теннис-трава-768x458.avif",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Давид Хорда Санчис — Джастин Энгель. ATP Challenger 125 Брауншвейг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243251&amp;hash=a9bc5837b6993d22\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Алекс Баррена — Роберто Карбальес-Баэна. ATP Challenger 125 Брауншвейг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243250&amp;hash=1deed2eee0fe297c\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Кэмерон Норри — Карлос Алькарас. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246519&amp;hash=7c53ed40e8eba7ed\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Аманда Анисимова — Анастасия Павлюченкова. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246520&amp;hash=d47821b19a7bba90\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Тимофей Дерепаско — Матисс Фарзам. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246640&amp;hash=ee01dc7f42f3088a\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Вероника Кудерметова/Элизе Мертенс — Габриэла Дабровски/Эрин Рутлифф. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246522&amp;hash=a66b76dc3e8ad7a8\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "19:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Снукер. Championship League 2025. Прямая трансляция 8 июля 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/XlPd7IniJkdx \" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/06/Снукер.-Championship-League-2025-768x420.avif",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "велоспорт",
+    "name": "Велогонка Тур де Франс 2025. 4-й этап. Прямая трансляция 8 июля 2025 в 14:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://plvideo.ru/embed/aG8eZTrGywyf\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.08",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "велоспорт",
+    "name": "Велоспорт. Джиро д’Италия 2025. Женщины. 3-й этап. Прямая трансляция 8 июля 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-227426668&amp;id=456239690&amp;hash=76e48231405cc622\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/07/Giro-dItalia-Women-768x352.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Марсело Аревало/Мате Павич — Юго Нис/Эдуар Роже-Васслен. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246640&amp;hash=ee01dc7f42f3088a\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Томас Мартин Этчеверри — Лука Павлович. ATP Challenger 125 Брауншвейг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243249&amp;hash=3cc325ca6e6b6df8\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Марк Вудфорд/Барбара Шетт — Ненад Зимонич/Мартина Навратилова. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/TelP5XE4RyDp \" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Ринки Хиджиката/Дэвид Пел — Рафаэль Матос/Марсело Мело. 1/4 финала Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246641&amp;hash=953f8f1aec56aa24\" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Сергей Лысов — Кейси Ратцлафф. Уимблдон",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/fD9CNIWpHOn_ \" width=\"640\"></iframe>",
+    "data": "2025.07.08",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
     "category": "теннис",
     "name": "Егор Плешивцев — Иван Иванов. Уимблдон",
     "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246640&amp;hash=ee01dc7f42f3088a\" width=\"640\"></iframe>",
@@ -22,26 +242,6 @@ export const transLinks =[
     "data": "2025.07.08",
     "time": "13:00",
     "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Клеман Табюр — Янник Ханфманн. ATP Challenger 125 Брауншвейг",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243248&amp;hash=714f87cbb7bfe28d\" width=\"640\"></iframe>",
-    "data": "2025.07.08",
-    "time": "11:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Велогонка Тур де Франс 2025. 4-й этап. Прямая трансляция 8 июля 2025 в 14:00",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" height=\"360 frameborder=\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/tnt1\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.07.08",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
     "premium": "",
     "active": 0
   }
