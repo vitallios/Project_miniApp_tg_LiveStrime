1f0884b608e57264903a31b9cd80a9ab6ebd9636
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-15 15:06:55 +0300

    add strime

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -4,44 +4,214 @@
 // 0 - трансляция закончилась
 // 1 - трансляция началась
 
-export const transLinks =[
+export const transLinks = [
   {
-    "category": "теннис",
-    "name": "Егор Плешивцев — Иван Иванов. Уимблдон",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246640&amp;hash=ee01dc7f42f3088a\" width=\"640\"></iframe>",
-    "data": "2025.07.08",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "category": "другое",
+    "name": "Смотреть Снукер. Championship League 2025. Прямая трансляция 15 июля 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/QZEnN0kgXT8\" title=\"WATCH LIVE | 2025 BetVictor Championship League Snooker Ranking Edition | Table One\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.15",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/06/Снукер.-Championship-League-2025-768x420.avif",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Смотреть Уимблдон 1/4 финала Прямая трансляция 8 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246641&amp;hash=953f8f1aec56aa24\" width=\"640\"></iframe>",
-    "data": "2025.07.08",
-    "time": "13:00",
-    "img": "https://srrb.ru/wp-content/uploads/2023/06/Уимблдон-768x432.jpg",
+    "name": "Николай Будков Крайер — Тьяго Монтейро. ATP 250 Бостад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"315\" src=\"https://plvideo.ru/embed/eRftgYIUWa3p\" width=\"560\"></iframe>",
+    "data": "2025.07.15",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Клеман Табюр — Янник Ханфманн. ATP Challenger 125 Брауншвейг",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243248&amp;hash=714f87cbb7bfe28d\" width=\"640\"></iframe>",
-    "data": "2025.07.08",
-    "time": "11:00",
+    "name": "Элиаш Имер — Тристан Бойер. ATP 250 Бостад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243325&amp;hash=2c925d939d568841\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть WWE Monday Night Raw 14.07.2025. Прямая трансляция",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-40248182&amp;id=456240964&amp;hash=16ec80d20d7cc845\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": null,
+    "img": "https://srrb.ru/wp-content/uploads/2024/06/WWE-Monday-Night-RAW-03.06.2024-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Нома Ноха Акуге — Анна Бондарь. WTA 250 Гамбург",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246679&amp;hash=926c1ad0563e0f7d\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "13:40",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Алина Корнеева — Манон Леонард. WTA 125 Порту",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/iW0IH0575y9d \" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "13:00",
     "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
     "premium": "",
     "active": 0
   },
   {
-    "category": "велоспорт",
-    "name": "Велогонка Тур де Франс 2025. 4-й этап. Прямая трансляция 8 июля 2025 в 14:00",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" height=\"360 frameborder=\" scrolling=\"no\" src=\"https://live.vkvideo.ru/app/embed/tnt1\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.07.08",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
+    "category": "теннис",
+    "name": "Мартин Ландалус-Лакамбра — Давид Гоффен. АТР 250 Гштаад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243324&amp;hash=de2d374c2225e008\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/гштат-768x284.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Хуго Гастон — Чун-Хсин Ценг. ATP 250 Бостад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"315\" src=\"https://plvideo.ru/embed/xnxXm5LXpG_N\" width=\"560\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Себастьян Офнер — Дамир Джумхур. ATP 250 Бостад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243323&amp;hash=c97d60f90c77ddb1\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Хуан-Мануэль Серундоло — Ян-Леннард Штруфф. АТР 250 Гштаад",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243322&amp;hash=848eca7471e76728\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "11:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/гштат-768x284.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Симона Вальтерт — Анастасия Севастова. WTA 250 Яссы",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/zP2Trr_gcfhT \" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-Яссы-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Элина Аванесян — Андрея Присэкариу. WTA 250 Яссы",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/N3Y7h6LHsEZw \" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-Яссы-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Александра Крунич — Далма Галфи. WTA 250 Гамбург",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246679&amp;hash=926c1ad0563e0f7d\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Сада Нахимана — Аранча Рус. WTA 250 Гамбург",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/TuU2ZdjITy_N \" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "11:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Мария Тимофеева — Кайя Юван. WTA 250 Гамбург",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246576&amp;hash=639bb47856f8238c\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "11:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть онлайн Водное поло. Хорватия — Венгрия. Женщины. Чемпионат Мира по водным видам спорта 2025. Прямой эфир 15 июля 2025.",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2219815\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.07.15",
+    "time": "15:40",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Мира-по-водным-видам-спорта-2025-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть онлайн Водное поло. Австралия — Новая Зеландия. Женщины. Чемпионат Мира по водным видам спорта 2025. Прямой эфир 15 июля 2025.",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2219812\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.07.15",
+    "time": "12:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Мира-по-водным-видам-спорта-2025-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Алибек Качмазов — Хуан Пабло Фикович. ATP 250 Лос-Кабос",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"428\" src=\"https://plvideo.ru/embed/5oFvSz_z5pmd\" width=\"760\"></iframe>",
+    "data": "2025.07.15",
+    "time": "07:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/ATP-250-Лос-Кабос-768x391.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Таро Даниэл — Алекс Эрнандес. ATP 250 Лос-Кабос",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243319&amp;hash=167486e3b3760d74\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "07:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/ATP-250-Лос-Кабос-768x391.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Александр Вукич — Эмилио Нава. ATP 250 Лос-Кабос",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243318&amp;hash=790db38a098ae7ee\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "05:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/ATP-250-Лос-Кабос-768x391.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Хэйди Хабиб — Александр Ковачевич. ATP 250 Лос-Кабос",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243317&amp;hash=9f14acfd89425114\" width=\"640\"></iframe>",
+    "data": "2025.07.15",
+    "time": "04:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/ATP-250-Лос-Кабос-768x391.jpg",
     "premium": "",
     "active": 0
   }
