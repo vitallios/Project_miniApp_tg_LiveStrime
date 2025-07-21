0a39f6df4aa6d1c87ba5925fd6d2af89c4e36fcd
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-21 12:24:48 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -4,183 +4,34 @@
 // 0 - трансляция закончилась
 // 1 - трансляция началась
 
-export const transLinks = [{
-    category: "регби",
-    name: "Тест-матч. Самоа - Шотландия.",
-    link: '<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456282680&hash=6dbf8fbf2c58209b" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    data: "2025.07.18",
-    time: "11:00",
-    img: "https://sun1-90.userapi.com/impg/ObcYqJtZQsHGWdN5nqYEc4TGcZQADlrFmtfi5g/o_9NBK7a-Ho.jpg?size=320x180&quality=95&sign=b8161011398f03bb852d1cab2c4f8aa4&c_uniq_tag=MVZNN5JgNUsi-0V90ur5xVct9zJYFzcrcvaWHc-vhhU&type=video_thumb",
-    premium: "premium",
-    active: 0,
-  },
+export const transLinks = [
   {
-    category: "регби",
-    name: "Товарищеский матч. Аргентина-А - Румыния.",
-    link: '<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456282680&hash=6dbf8fbf2c58209b" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    data: "2025.07.18",
-    time: "21:00",
-    img: "https://sun9-25.userapi.com/impg/C4Bs3rUFa4KMjs56g1ml7qF2GM09-r8mk81MLQ/7SmsXnjDm7Y.jpg?size=1280x720&quality=95&sign=b124666c78a73d45e5693462e800a6fb&type=video_thumb",
-    premium: "premium",
-    active: 0,
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть WRC. Ралли Эстонии. SSS 1",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-140964975&amp;id=456241644&amp;hash=e2af78681a67b996\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/04/WRC.-2025-768x416.jpg",
+    "category": "теннис",
+    "name": "Мари Бузкова — Синю Гао. WTA250 Прага",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/E84pn0q0ElEY \" width=\"640\"></iframe>",
+    "data": "2025.07.21",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA250-Прага-768x425.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Надежда Киченок/Макото Ниномия — Моюка Учижима/Чжэн Сайсай. 1/2 финала WTA 250 Гамбург",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/0ACjeehC9EhK \" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть ONE Friday Fights 116: Адам vs Нахайян Мохаммед. Прямая трансляция 18 июля 2025 в 15:30.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"405\" src=\"https://www.youtube.com/embed/PAxAKpjhwnc\" title=\"🔴 [Live In HD] ONE Friday Fights 113: Donking vs. Yodlekpet\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.07.18",
-    "time": "15:30",
-    "img": "https://sport-lives.ru/wp-content/uploads/2024/11/one-fc.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть Прыжки в воду. Летняя Универсиада 2025. Прямая трансляция 18 июля 2025.",
-    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2220225\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:30",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/летняя-универсиада-2025-768x420.jpg",
+    "name": "Александр Шевченко — Даниэль Галан. ATP250 Китцбюэль",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://plvideo.ru/embed/iRuUD2B7kifP\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.21",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Китцбюэль-768x447.jpg",
     "premium": "",
     "active": 0
   },
   {
     "category": "теннис",
-    "name": "Луиз Буассон — Виктория Томова. 1/4 финала WTA 250 Гамбург",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246601&amp;hash=54d0bd95c35d30ed\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "18:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Даяна Ястремская — Далма Галфи. 1/4 финала WTA 250 Гамбург",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/KjwerGglf91V \" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Екатерина Александрова — Анна Бондарь. 1/4 финала WTA 250 Гамбург",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-218116775&amp;id=456246600&amp;hash=1b9b24db4b9f71d5\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "16:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA-250-🇩🇪-Гамбург-768x292.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Виталия Дьяченко — Химено Сакацуме. 1/4 финала WTA 125 Порту",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/rE0yDbP_mlSR \" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Джером Ким — Артюр Казо. 1/4 финала АТР 250 Гштаад",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243388&amp;hash=fd1efd05d3e40f41\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "18:30",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/гштат-768x284.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Франсиско Комесанья — Александр Бублик. 1/4 финала АТР 250 Гштаад",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243384&amp;hash=e0597e48d2b082e5\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:30",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/гштат-768x284.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Франциско Серундоло — Дамир Джумхур. 1/4 финала ATP 250 Бостад",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243387&amp;hash=1cba0dfd8696b046\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "16:30",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "теннис",
-    "name": "Себастьян Баэс — Лучано Дардери. 1/4 финала ATP 250 Бостад",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243385&amp;hash=f0143f410b33f4d0\" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/бастад-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть Снукер. Championship League 2025. Прямая трансляция 18 июля 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/_bgdCO4sTb6N \" width=\"640\"></iframe>",
-    "data": "2025.07.18",
-    "time": "14:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/06/Снукер.-Championship-League-2025-768x420.avif",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть онлайн Водное поло. Канада — Черногория. Плей-офф Чемпионата Мира по водным видам спорта 2025. Прямой эфир 18 июля 2025.",
-    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2219865\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
-    "data": "2025.07.18",
-    "time": "15:45",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Мира-по-водным-видам-спорта-2025-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "другое",
-    "name": "Смотреть онлайн Водное поло. Бразилия — Греция. Плей-офф Чемпионата Мира по водным видам спорта 2025. Прямой эфир 18 июля 2025.",
-    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2219861\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
-    "data": "2025.07.18",
-    "time": "14:05",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/Чемпионат-Мира-по-водным-видам-спорта-2025-768x432.jpg",
-    "premium": "",
-    "active": 0
-  },
-  {
-    "category": "велоспорт",
-    "name": "Смотреть Тур де Франс 2025. 13-й этап. Прямая трансляция 18 июля 2025 в 13:45",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://plvideo.ru/embed/c-9Ye4Cvkcno\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
-    "data": "2025.07.18",
-    "time": "13:45",
-    "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
+    "name": "Франсиско Комесанья — Тристан Бойер. ATP250 Китцбюэль",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243433&amp;hash=5b0928b5dad105a1\" width=\"640\"></iframe>",
+    "data": "2025.07.21",
+    "time": "12:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Китцбюэль-768x447.jpg",
     "premium": "",
     "active": 0
   }
