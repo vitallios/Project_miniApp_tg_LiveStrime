22b263afe1c8c281cc05b35422b7dcfc3e00f744
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-25 16:12:14 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -4,8 +4,7 @@
 // 0 - трансляция закончилась
 // 1 - трансляция началась
 
-export const transLinks = [
-  {
+export const transLinks = [{
     "category": "Футбол",
     "name": "Академия футбола (Тамбов) - Рязань-2 (Рязань)",
     "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240822&hash=f26a5c3d5051a0ba" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
@@ -153,13 +152,183 @@ export const transLinks = [
     "category": 'Другое',
     "name": 'West Siberian Championship, день 2',
     "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-215240369&id=456239120&hash=9464968ccda8d6aa" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    "data": '2025.07.25',
+    "data": '2025.07.26',
     "time": '07:00',
     "img": 'https://sun1-85.userapi.com/impg/XZjaG8Zw3DqNFCThK6aS2a6aS5QZ9yi_qYD9tw/Hk0WDYiW53s.jpg?size=320x128&quality=95&sign=99a1abd8089b0e639cd7b3c29b6ab193&c_uniq_tag=WAF63UD2Uj2c93jHq4Hv84r2JmmRgiWjnGsNxqLJswk&type=video_thumb',
     "premium": '',
     "active": 0,
     "allDay": 'all day'
   },
+  {
+    "category": "теннис",
+    "name": "Александр Бублик — Ботик Ван де Зандсхулп. 1/2 финала ATP250 Китцбюэль",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243537&amp;hash=fa3044e663277a10\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "15:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Китцбюэль-768x447.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "велоспорт",
+    "name": "Смотреть Тур де Франс 2025. 19-й этап. Прямая трансляция 25 июля 2025 в 14:15",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://plvideo.ru/embed/7m7L2X6aeYEF\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.25",
+    "time": "14:15",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/тур-де-франс-768x428.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Фрайбург — Динамо Дрезден. Товарищеский матч. Прямая трансляция 25 июля 2025 в 15:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/SYrlArN9Mysw \" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/футбольный-мяч-768x399.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Мари Боузкова — Тереза Валентова. 1/2 финала WTA250 Прага",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246724&amp;hash=63ca7fb925dfafd0\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA250-Прага-768x425.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть ONE Friday Fights 117: Yod-IQ vs Малатеста. Прямая трансляция 25 июля 2025 в 15:30.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://www.youtube.com/embed/TkH64eLNvgE\" title=\"🔴 [Live in HD] ONE Friday Fights 117: Yod-IQ vs. Malatesta\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.25",
+    "time": "15:30",
+    "img": "https://srrb.ru/wp-content/uploads/2023/05/one-fc-768x450.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Пул. Чемпионат мира. 5-й день. Прямая трансляция 25 июля 2025 в 14:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/00V0974LW3k\" title=\"WATCH LIVE | 2025 World Pool Championship | Table Four | Day Five\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.07.25",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/02/бильярд-пул-768x402.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Жомбор Пирош — Стефанос Сакелларидис. 1/4 финала Challenger 125 Цуг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243538&amp;hash=3f101dd42804d184\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Факундо Диас Акоста — Якуб Пол. 1/4 финала Challenger 125 Цуг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243539&amp;hash=a0b474856f8f2cdf\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Арольд Майо — Мика Брунольд. 1/4 финала Challenger 125 Цуг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243536&amp;hash=8f42630bc37fd9e0\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/12/теннис-ракетки.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Карлос Табернер — Дамир Джумхур. 1/2 финала ATP250 Умаг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243541&amp;hash=68a33ffc7dc10b60\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "22:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/умаг-768x388.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Камильо Уго Карабелли — Лучано Дардери. 1/2 финала ATP250 Умаг",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243540&amp;hash=7445421dba57fda2\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "19:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/умаг-768x388.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Линда Носкова — Синьюй Ван. 1/2 финала WTA250 Прага",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-144358990&amp;id=456246724&amp;hash=63ca7fb925dfafd0\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA250-Прага-768x425.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Формула-1. Гран-при Бельгии 2025. Квалификация к спринту. Прямая трансляция 25 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/0b2ec38a3c812a36444d614809ebe274/\" width=\"720\"></iframe>",
+    "data": "2025.07.25",
+    "time": "17:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Формула-1-Гран-при-Бельгии-2025-768x426.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть онлайн Формула-1. Гран-при Бельгии 2025. Практика. Прямой эфир 25 июля 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"405\" src=\"https://rutube.ru/play/embed/d12b5afba7d57ec8ccdfcabbc6d5cbe9/\" width=\"720\"></iframe>",
+    "data": "2025.07.25",
+    "time": "13:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Формула-1-Гран-при-Бельгии-2025-768x426.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Карпаты Львов — Лестер. Товарищеский матч. Прямая трансляция 25 июля 2025 в 13:30",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/lV3i0VXKx-F2 \" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "13:30",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/футбольный-мяч-768x399.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "Артюр Казо — Артур Риндеркнеш. 1/2 финала ATP250 Китцбюэль",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-226610789&amp;id=456243535&amp;hash=fba52fbffe80e6d1\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "13:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/Китцбюэль-768x447.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "теннис",
+    "name": "🇬🇧 Эмили Эпплтон/🇳🇱 Изабель Ховерлаг — 🇺🇦 Надежда Киченок/🇯🇵 Макото Ниномия. 1/2 финала WTA250 Прага",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212808533&amp;id=456259167&amp;hash=eb4b51ff17240f11\" width=\"640\"></iframe>",
+    "data": "2025.07.25",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/WTA250-Прага-768x425.jpg",
+    "premium": "",
+    "active": 0
+  }
 
 ]
 
@@ -179,4 +348,3 @@ export const transLinks = [
 //   premium: "premium",
 //   active: 0,
 // },
-
