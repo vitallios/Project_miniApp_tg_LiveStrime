637454298066b0c76eb7a14dafb36508cac14256
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-07-25 10:38:00 +0300

    add footbol

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,13 +6,93 @@
 
 export const transLinks = [
   {
-    "category": "регби",
-    "name": "Сборная коренных народов Австралии и ТР - Британские и Ирландские Львы.",
-    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-200149158&id=456282794&hash=e56bb147a6eef072" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    "data": "2025.07.22",
+    "category": "Футбол",
+    "name": "Академия футбола (Тамбов) - Рязань-2 (Рязань)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240822&hash=f26a5c3d5051a0ba" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.28",
+    "time": "16:00",
+    "img": "https://sun9-49.userapi.com/impg/P15CbgFcZry6BFKH33L3Xsdhx4oktEgjEvPAMA/xm5yIRqJR8s.jpg?size=1280x720&quality=95&sign=66c540a6dd926419ec992433471a15c7&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Атом (Нововоронеж) - Металлург-М (Липецк)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240823&hash=a4041677f93d736c" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.27",
+    "time": "17:00",
+    "img": "https://sun9-83.userapi.com/impg/sJw9uqkzQQRbBNXQGh-MMgLG6ve-lYcKtyQ0-w/Gnw71OZDxvE.jpg?size=1280x720&quality=95&sign=d4ceb9c39645500f78717212f9f76763&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Авангард-М (Курск) - СШОР №1 Салют-М (Белгород)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240824&hash=98881879b35aff03" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.27",
+    "time": "15:00",
+    "img": "https://sun9-11.userapi.com/impg/Q2UjIIN44zKLVTfzWTk58_xIIJNn17HeVtDHLg/2CwQXreYaVI.jpg?size=1280x720&quality=95&sign=530b95a6366f100b091671b283bc16c6&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Калуга-М (Калуга) - Динамо-Брянск-М (Брянск)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240825&hash=ba1bd658078c9923" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.27",
+    "time": "14:00",
+    "img": "https://sun9-18.userapi.com/impg/fmiCZlwk_2op8PYUtuRip5y2gqmnDo3676L6Aw/hhaAXLwxI6o.jpg?size=1280x720&quality=95&sign=a567a4fcd21b98e59eb8dafd7231efd3&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "БГУОР-Спартак (Брянск) - ЛеидТоги (Курск)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240826&hash=efd020b09c881892" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.26",
     "time": "13:00",
-    "img": "https://img.championat.com/s/732x488/news/big/z/m/chto-stoit-znat-o-britanskih-i-irlandskih-lvah_1753008580619649123.jpg",
-    "premium": "premium",
+    "img": "https://sun9-25.userapi.com/impg/8s3_PnVvdYJSGei8ch-xW7igsWvtiOgwohnXhA/3mRUB6-zdzU.jpg?size=1280x720&quality=95&sign=ab90a9ffbacddf52567077b34bf9f5dd&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Орёл-М (Орёл) - Университет спорта (Смоленская область)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240830&hash=3d1c6d77ff7a88cc" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.26",
+    "time": "18:00",
+    "img": "https://sun1-92.userapi.com/impg/3l5FJEybB_uMOist4tNnvUvd_Ekggnqfmyd8fQ/WbFZ_xGq800.jpg?size=1280x720&quality=95&sign=5c03eb0c9d7467c156216351daf72384&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Фортуна (Боровск) - Днепр (Смоленская область)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240829&hash=1a5dd844b94164bf" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.26",
+    "time": "16:00",
+    "img": "https://sun9-54.userapi.com/impg/DPzq3zM5JOmzTkVLvRonLt4CXn-wyw8Ok8Y_EQ/2liSqjb11Is.jpg?size=1280x720&quality=95&sign=37dd15ef24d0af0bff0e326c64b052c4&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Кристалл-МЭЗТ (Борисоглебск) - Факел-2 (Воронеж)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240828&hash=46690ef39cbb9bc0" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.26",
+    "time": "15:00",
+    "img": "https://sun9-12.userapi.com/impg/rJqdCEyrWjF-dBe531YKeDYojwNvVHaLJt0O2w/P1f2wNAnlb0.jpg?size=1280x720&quality=95&sign=1e5315b321d1f24297fc605f2271a105&type=video_thumb",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "Футбол",
+    "name": "Елец (Елец) - Металлург-Оскол (Старый Оскол)",
+    "link": '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240827&hash=5d5e65e747742d89" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+    "data": "2025.07.26",
+    "time": "14:00",
+    "img": "https://sun9-5.userapi.com/impg/6dfAB-5SoUsqQ64MXjcgFXNA0zHdJ7jilLMTQA/0SZG-I9dT8Q.jpg?size=1280x720&quality=95&sign=ffca9bd3fdbde1a7c9e3ec8162c76c02&type=video_thumb",
+    "premium": "",
     "active": 0
   },
 
