bb51d945a4e7be32d971198764a68beea3bbca38
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-08-06 10:06:41 +0300

    ups

--- live_failse/liveFilms.js
+++ live_failse/liveFilms.js
@@ -1,9 +1,8 @@
 // Фильмы
-export const randomFilms = [
- {
-   id: 1,
-   name: "Битлджус Битлджус | Beetlejuice Beetlejuice (2024)",
-   link: `<iframe
+export const randomFilms = [{
+    id: 1,
+    name: "Битлджус Битлджус | Beetlejuice Beetlejuice (2024)",
+    link: `<iframe
    src="https://rutube.ru/play/embed/0b87b1557018d8ff86c86cf3492ad5a8"
    frameBorder="0"
    allow="clipboard-write; autoplay"
@@ -12,12 +11,12 @@ export const randomFilms = [
    allowFullScreen
  ></iframe>
 `,
-   // link: '2',
- },
- {
-   id: 2,
-   name: "Чужой: Ромул / Alien: Romulus (2024)",
-   link: `
+    // link: '2',
+  },
+  {
+    id: 2,
+    name: "Чужой: Ромул / Alien: Romulus (2024)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/edf4662bfdf97433ced02ab6154313c4"
        frameBorder="0"
@@ -27,12 +26,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 3,
-   name: "Дэдпул и Росомаха (2024)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 3,
+    name: "Дэдпул и Росомаха (2024)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/f893c0764662db0b17b277d15d6e0871"
        frameBorder="0"
@@ -42,12 +41,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 4,
-   name: "Джокер (2019)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 4,
+    name: "Джокер (2019)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/81e784b6eba4c9d3d0539536d7e58243"
        frameBorder="0"
@@ -57,12 +56,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 5,
-   name: "Криминальное чтиво (1994)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 5,
+    name: "Криминальное чтиво (1994)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/b655f8af33deef0514481b2bb39b90e2"
        frameBorder="0"
@@ -72,12 +71,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 6,
-   name: "Оппенгеймер (2023)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 6,
+    name: "Оппенгеймер (2023)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/ffe77a61fb1ab119c33638900d8a77e4"
        frameBorder="0"
@@ -87,12 +86,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 7,
-   name: "Дом у дороги (2024)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 7,
+    name: "Дом у дороги (2024)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/22bd521930719eedd02383e9c81b16e2"
        frameBorder="0"
@@ -102,12 +101,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-   id: 8,
-   name: "Меч короля Артура (2017)",
-   link: `
+    // link: '1',
+  },
+  {
+    id: 8,
+    name: "Меч короля Артура (2017)",
+    link: `
      <iframe
        src="https://rutube.ru/play/embed/129b5b143a47f3d208b5aa151c72780b"
        frameBorder="0"
@@ -117,12 +116,12 @@ export const randomFilms = [
        allowFullScreen
      ></iframe>
    `,
-   // link: '1',
- },
- {
-  id: 9,
-  name: "Крэйвен-охотник (2024)",
-  link: `
+    // link: '1',
+  },
+  {
+    id: 9,
+    name: "Крэйвен-охотник (2024)",
+    link: `
       <iframe
         src="https://rutube.ru/play/embed/17d090183bb12968d3055a8da7dfccd6"
         frameBorder="0"
@@ -132,11 +131,11 @@ export const randomFilms = [
         allowFullScreen
       ></iframe>
     `
- },
- {
-  id: 10,
-  name: "Джон Уик: Балерина (HD) (2025)",
-  link: `
+  },
+  {
+    id: 10,
+    name: "Джон Уик: Балерина (HD) (2025)",
+    link: `
      <iframe 
      src="https://vkvideo.ru/video_ext.php?oid=-231952439&id=456239026&hash=a358bdbf930ecf9f"
      frameborder="0" 
@@ -147,20 +146,22 @@ export const randomFilms = [
      picture-in-picture">
     </iframe>
     `
- },
- {
-  id: 11,
-  name: "Ледяной драйв 2: Возмездие",
-  link: `
-      <iframe
-        src="https://t.me/archive_kinozona_1080p/196"
-        frameBorder="0"
-        allow="clipboard-write; autoplay"
-        webkitAllowFullScreen
-        mozallowfullscreen
-        allowFullScreen
-      ></iframe>
+  }, {
+    id: 11,
+    name: "Ледяной драйв 2: Возмездие",
+    link: `
+    <iframe 
+      src="https://vkvideo.ru/video_ext.php?oid=-220018529&id=456247188&hash=644c59017d83e3ba" 
+      frameborder="0" 
+      allowfullscreen="1" 
+      allow="
+      autoplay; 
+      encrypted-media; 
+      fullscreen; 
+      picture-in-picture
+      ">
+    </iframe>
     `
- }
+  }
 ];
 // https://t.me/archive_kinozona_1080p/196
