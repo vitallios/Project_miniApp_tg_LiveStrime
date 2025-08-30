7aa6a27134dec675f6029c2d892836ee3525c574
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-08-30 10:31:18 +0300

    Update liveTranslation.js

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -7,28 +7,245 @@
 export const transLinks = 
 [
   {
-    category: "футбол",
-    name: "ЗАРЯ ЛУГАНСК - ПСК Финал кубка ЮФО/СКФО",
-    link: '<iframe src="https://vkvideo.ru/video_ext.php?oid=-224207689&id=456239802&hash=208688a6d3ccdb47" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    data: "2025.08.26",
-    time: "16:20",
-    img: "https://sun9-18.userapi.com/impg/5lCFV5KgsNiEdvMUP1MO2CP0-ioNBczni51U8Q/GKAMq7TLN00.jpg?size=320x180&quality=95&sign=cc67823ef367964a5598703f62f3f6fa&c_uniq_tag=TLWvESiG9TkNjGeTvvHtOiylUK9o-fFORxOPTio3VYI&type=video_thumb",
-    premium: "",
-    allDays: "",
-    active: 0
+    "category": "футбол",
+    "name": "Смотреть Пиза — Рома. Чемпионат Италии. Прямая трансляция 30 августа 2025 в 21:50",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234602\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "21:50",
+    "img": "https://srrb.ru/wp-content/uploads/2022/05/Чемпионат-Италии-по-футболу-768x411.jpg",
+    "premium": "",
+    "active": 0
   },
   {
-    category: "футбол",
-    name: "Орёл-М (Орёл) - Факел-2 (Воронеж)",
-    link: '<iframe src="https://vkvideo.ru/video_ext.php?oid=-157749661&id=456240866&hash=b045fa2ab7afd358" width="640" height="360" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media; fullscreen; picture-in-picture"></iframe>',
-    data: "2025.08.26",
-    time: "13:55",
-    img: "https://sun9-3.userapi.com/impg/ihiZo-2F05Rb1EZ6zTkfldWTlsc8PfNu4nWeFA/y4bA40inzxs.jpg?size=1280x720&quality=95&sign=65f4d264ed705eef9ba366272e5f0fca&type=video_thumb",
-    premium: "",
-    allDays: "",
-    active: 0
+    "category": "футбол",
+    "name": "Смотреть Наполи — Кальяри. Чемпионат Италии. Прямая трансляция 30 августа 2025 в 21:50",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234600\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "21:50",
+    "img": "https://srrb.ru/wp-content/uploads/2022/05/Чемпионат-Италии-по-футболу-768x411.jpg",
+    "premium": "",
+    "active": 0
   },
-  
+  {
+    "category": "футбол",
+    "name": "Смотреть Болонья — Комо. Чемпионат Италии. Прямая трансляция 30 августа 2025 в 19:30",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234590\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "19:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/05/Чемпионат-Италии-по-футболу-768x411.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Парма — Аталанта. Чемпионат Италии. Прямая трансляция 30 августа 2025 в 19:30",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234587\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "19:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/05/Чемпионат-Италии-по-футболу-768x411.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Реал — Мальорка. Чемпионат Испании. Прямая трансляция 30 августа 2025 в 22:30",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234605\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "22:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/01/Ла-Лига-2021-22-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Овьедо — Реал Сосьедад. Чемпионат Испании. Прямая трансляция 30 августа 2025 в 20:00",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234776\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "20:00",
+    "img": "https://srrb.ru/wp-content/uploads/2022/01/Ла-Лига-2021-22-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Алавес — Атлетико. Чемпионат Испании. Прямая трансляция 30 августа 2025 в 18:00",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2234580\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2022/01/Ла-Лига-2021-22-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Кастельон — Сарагоса. Чемпионат Испании. Сегунда. Прямая трансляция 30 августа 2025 в 22:30",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2235005\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.08.30",
+    "time": "22:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/01/Ла-Лига-2021-22-768x432.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Кубань – Калуга. Вторая Лига А. Прямая трансляция 30 августа 2025 в 19:00.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244879&amp;hash=d652d383b44d0a70\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "19:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Муром – Авангард К. Вторая Лига А. Прямая трансляция 30 августа 2025 в 17:00.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244878&amp;hash=5ebef67971785bdb\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Родина-2 – Динамо-Владивосток. Вторая Лига А. Прямая трансляция 30 августа 2025 в 15:00.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244877&amp;hash=597a8e164e759d9c\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Иртыш – Торпедо Миасс. Вторая Лига А. Прямая трансляция 30 августа 2025 в 14:00.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-139174916&amp;id=456244876&amp;hash=1358599873f4f73e\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Сочи-2 – Ростов-2. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 19:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244916&amp;hash=9fec575a18afdf2f\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "19:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Севастополь – Динамо Ставрополь. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 19:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244915&amp;hash=e874365b54171f5c\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "19:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Нарт – Победа. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 18:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244913&amp;hash=321e28cf049cfc3b\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Дружба – Динамо-2 Мх. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 18:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244914&amp;hash=ff908f1014419064\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "18:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Рязань – Знамя Труда. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244926&amp;hash=ce59d02f68ee151f\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Снукер. Финал Wuhan Open 2025. Прямая трансляция 30 августа 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\" https://plvideo.ru/embed/3J4j4PDW0XZC \" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/08/ухань-опен-768x422.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Спартак-Нальчик – Рубин Ялта. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244912&amp;hash=0c9216ac73ff8193\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Динамо-Брянск – Металлург Лп. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 16:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244925&amp;hash=55561b761b522a85\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Волна – Челябинск-2. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 16:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244931&amp;hash=3695f32b2b64dcb0\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Ангушт – Кубань Холдинг. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 16:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244911&amp;hash=285a190fee40d575\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Легион – Астрахань. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 16:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244910&amp;hash=60abb5c9e26a5d30\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Арсенал-2 – Динамо-СПб. Вторая Лига Б. Прямая трансляция 30 августа 2025 в 14:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-221212132&amp;id=456244924&amp;hash=1659b19eb484fbbb\" width=\"640\"></iframe>",
+    "data": "2025.08.30",
+    "time": "14:00",
+    "img": "https://srrb.ru/wp-content/uploads/2023/10/Футбол-Вторая-лига-768x449.jpg",
+    "premium": "",
+    "active": 0
+  }
 ]
 
 
