792ca25250aaae4ea96a0b8b5500637741df459b
Vitallios <61820576+vitallios@users.noreply.github.com>
2025-09-04 12:35:53 +0300

    update

--- live_failse/liveTranslation.js
+++ live_failse/liveTranslation.js
@@ -6,35 +6,68 @@
 
 export const transLinks = 
 [
+  {
+    "category": "велоспорт",
+    "name": "Смотреть Велоспорт. Тур Британии. 3-й этап. Прямая трансляция 4 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-227426668&amp;id=456239762&amp;hash=5b33bd757e8d7a76\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "13:30",
+    "img": "https://srrb.ru/wp-content/uploads/2024/03/велоспорт-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть США – Турция. 1/4 финала Чемпионата мира 2025 по волейболу. Женщины. 4 сентября 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=63444727\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/09/ВОЛЕЙБОЛ-ЧМ-768x415.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "другое",
+    "name": "Смотреть Бразилия – Франция. 1/4 финала Чемпионата мира 2025 по волейболу. Женщины. 4 сентября 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"600\" scrolling=\"no\" src=\"https://widgets.media.sportradar.com/uscommon/ru/betproxy/match.lmtplus/lmt1#matchId=63402351\" width=\"720\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "13:00",
+    "img": "https://srrb.ru/wp-content/uploads/2022/09/ВОЛЕЙБОЛ-ЧМ-768x415.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
 
   {
     "category": "другое",
-    "name": "Снукер. Xi’an Grand Prix 2025. Квалификация. Прямая трансляция 3 сентября 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-228871432&amp;id=456240641&amp;hash=110a90b7eb0ef2eb\" width=\"640\"></iframe>",
-    "data": "2025.09.03",
+    "name": "Снукер. Northern Ireland Open 2025. Квалификация. Прямая трансляция 4 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vk.com/video_ext.php?oid=-211669815&amp;id=456249936&amp;hash=0997aab672961385\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
     "time": "12:00",
-    "img": "https://srrb.ru/wp-content/uploads/2024/07/Xian-Grand-Prix-2024-768x384.jpg",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/Northern-Ireland-Open-2025-768x391.jpg",
     "premium": "",
     "allDays": "",
     "active": 0
   },
   {
     "category": "другое",
-    "name": "Смотреть Первенство города Москвы 2025 по фигурному катанию. Прямая трансляция 3 сентября 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-44553412&amp;id=456239088&amp;hash=72d542bd3664078c\" width=\"640\"></iframe>",
-    "data": "2025.09.03",
-    "time": "11:45",
-    "img": "https://srrb.ru/wp-content/uploads/2021/10/фигурное-катание-768x432.jpg",
+    "name": "Смотреть Фигурное катание. 3-й этап ИСУ Гран-при Юниоры 2025. Варезе. Прямая трансляция 4 — 6 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/Yt5UDsktn80\" title=\"Junior Ice Dance Rhythm Dance | Varese 2025 | #JGPFigure\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2022/11/ИСУ-серия-Фигурное-катание-768x433.jpg",
     "premium": "",
     "allDays": "",
     "active": 0
   },
   {
     "category": "другое",
-    "name": "Смотреть 1-й этап Кубка Санкт-Петербурга 2025 по фигурному катанию. Прямая трансляция 3 сентября 2025.",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-78970490&amp;id=456240387&amp;hash=ae2537e8745510a8\" width=\"640\"></iframe>",
-    "data": "2025.09.03",
-    "time": "11:00",
+    "name": "Смотреть Первенство города Москвы 2025 по фигурному катанию. Прямая трансляция 4 сентября 2025.",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-44553412&amp;id=456239089&amp;hash=953943d5a0ac322a\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "11:20",
     "img": "https://srrb.ru/wp-content/uploads/2021/10/фигурное-катание-768x432.jpg",
     "premium": "",
     "allDays": "",
@@ -42,27 +75,138 @@ export const transLinks =
   },
   {
     "category": "хоккей",
-    "name": "Смотреть Кузбасс  — Саяны. ЧР 2025/2026 по хоккею с мячом. Прямая трансляция 3 сентября 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-217393213&amp;id=456241100&amp;hash=dea98bc62642eb76\" width=\"640\"></iframe>",
-    "data": "2025.09.03",
-    "time": "15:00",
-    "img": "https://srrb.ru/wp-content/uploads/2021/08/Хоккей-с-мячом-768x422.jpg",
+    "name": "Смотреть Красная Армия – МХК Динамо СПб. МХЛ 2025/26. Прямая трансляция 4 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/i6I6VpTrc7g\" title=\"4.09.2025. «Красная Армия» – МХК «Динамо» СПб | (OLIMPBET МХЛ 25/26) – Прямая трансляция\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "18:30",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/мхл-лого-768x432.jpg",
     "premium": "",
     "allDays": "",
     "active": 0
   },
   {
-    "category": "теннис",
-    "name": "Смотреть US Open 2025. Прямая трансляция 2 — 3 сентября 2025",
-    "link": "<iframe allowfullscreen=\"allowfullscreen\" data-mce-fragment=\"1\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-212808533&amp;id=456260026&amp;hash=dc97ad9a2c0778e6\" width=\"640\"></iframe>",
-    "data": "2025.09.03",
-    "time": "18:00",
-    "img": "https://srrb.ru/wp-content/uploads/2025/08/US-Open-2025-768x424.jpg",
+    "category": "хоккей",
+    "name": "Смотреть МХК Спартак MAX – ХК Капитан. МХЛ 2025/26. Прямая трансляция 4 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/8qa8pduY44U\" title=\"4.09.2025. МХК «Спартак МАХ» – ХК «Капитан» | (OLIMPBET МХЛ 25/26) – Прямая трансляция\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "18:30",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/мхл-лого-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "Смотреть Кузнецкие Медведи – Авто. МХЛ 2025/26. Прямая трансляция 4 сентября 2025",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://www.youtube.com/embed/qzBgEQF6jG0\" title=\"4.09.2025. «Кузнецкие Медведи» – «Авто» | (OLIMPBET МХЛ 25/26) – Прямая трансляция\" width=\"640\"><span class=\"mce_SELRES_start\" data-mce-type=\"bookmark\" style=\"display: inline-block; width: 0px; overflow: hidden; line-height: 0;\">﻿</span></iframe>",
+    "data": "2025.09.04",
+    "time": "14:30",
+    "img": "https://srrb.ru/wp-content/uploads/2020/09/мхл-лого-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Спартак Кс – Урал. Первая Лига. Прямая трансляция 4 сентября 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-29484355&amp;id=456250231&amp;hash=5100ed7c1a96fd5e\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/первая-лига-768x435.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "футбол",
+    "name": "Смотреть Челябинск – Факел. Первая Лига. Прямая трансляция 4 сентября 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-29484355&amp;id=456250232&amp;hash=ffcc9a1996d92241\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/07/первая-лига-768x435.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "Магнитка – Олимпия. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 17:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257255&amp;hash=d6a74a5cdf3ce6fb\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "17:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "Южный Урал – Молот. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 16:30",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257254&amp;hash=9a6e6133c3746e6c\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "Зауралье – ЦСК ВВС. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 16:30",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257253&amp;hash=130263e1fac89418\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "16:30",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "Омские Крылья – Барс. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 16:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257252&amp;hash=e9a8b94db6705524\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "16:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+  {
+    "category": "хоккей",
+    "name": "ХК Норильск – Динамо-Алтай. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 15:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257250&amp;hash=538625983e062d2d\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+
+  {
+    "category": "хоккей",
+    "name": "Смотреть Сокол – Металлург Нк. ВХЛ 2025/2026. Прямая трансляция 4 сентября 2025 в 15:00",
+    "link": "<iframe allowfullscreen=\"allowfullscreen\" frameborder=\"0\" height=\"360\" src=\"https://vkvideo.ru/video_ext.php?oid=-150501268&amp;id=456257251&amp;hash=c1971729f2e5d325\" width=\"640\"></iframe>",
+    "data": "2025.09.04",
+    "time": "15:00",
+    "img": "https://srrb.ru/wp-content/uploads/2025/09/вхл-768x432.jpg",
+    "premium": "",
+    "allDays": "",
+    "active": 0
+  },
+
+  {
+    "category": "другое",
+    "name": "Смотреть Кубок губернатора Югры 2025. Выездка. Прямая трансляция 4 сентября 2025.",
+    "link": "<iframe frameborder=\"0\" height=\"405\" scrolling=\"no\" src=\"https://news.sportbox.ru/vdl/poster/2237777\" style=\"width: 100%; max-width: 720px;\" width=\"720\"></iframe>",
+    "data": "2025.09.04",
+    "time": "06:00",
+    "img": "https://srrb.ru/wp-content/uploads/2024/09/Конный-спорт.-Кубок-губернатора-Югры-2024.-Всероссийские-соревнования-по-выездке-и-конкуру-768x444.jpg",
     "premium": "",
     "allDays": "",
     "active": 0
   }
-
 ]
 
 
